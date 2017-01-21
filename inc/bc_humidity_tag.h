03872d891d9c024be6f3df5c64342244d298f9a4
Petr Konvicka <petr.konvicka@bigclown.com>
2017-01-21 20:02:58 +0100

    function matrix added to pseudo-wrapper

--- inc/bc_humidity_tag.h
+++ inc/bc_humidity_tag.h
@@ -1,36 +1,27 @@
 #ifndef _BC_HUMIDITY_TAG_H
 #define _BC_HUMIDITY_TAG_H
 
+#include <stddef.h>
 #include <bc_hdc2080.h>
 #include <bc_hts221.h>
+#include <bc_common.h>
+#include <bc_tick.h>
 
 typedef enum
 {
     BC_HUMIDITY_TAG_DEVICE_HTS221 = 0,
-    BC_HUMIDITY_TAG_DEVICE_HDC2080
+    BC_HUMIDITY_TAG_DEVICE_HDC2080,
+    BC_HUMIDITY_TAG_DEVICE_COUNT
 
 } bc_humidity_tag_device_t;
 
 typedef enum
 {
     BC_HUMIDITY_TAG_I2C_ADDRESS_DEFAULT = 0,
-    BC_HUMIDITY_TAG_I2C_ADDRESS_ALTERNATE = 1
+    BC_HUMIDITY_TAG_I2C_ADDRESS_ALTERNATE,
+    BC_HUMIDITY_TAG_I2C_ADDRESS_COUNT
 
 } bc_humidity_tag_i2c_address_t;
-
-typedef enum
-{
-    BC_HTS221_I2C_ADDRESS_DEFAULT = 0x5f,
-    BC_HTS221_I2C_ADDRESS_ALTERNATE = 0x40
-
-} bc_hts221_i2c_address_t;
-
-typedef enum
-{
-    BC_HDC2080_I2C_ADDRESS_DEFAULT = 0x40,
-    BC_HDC2080_I2C_ADDRESS_ALTERNATE = 0x41
-
-} bc_hdc2080_i2c_address_t;
 
 /*
  typedef bc_hdc2080_event_t bc_humidity_tag_event_t;
@@ -53,105 +44,13 @@ typedef union
     bc_hdc2080_event_t hdc2080_event;
 } bc_humidity_tag_event_t;
 
-// TODO ... if, else if, else ... nahradit matic9 funkc9
+// TODO ... doporuèuju nepoužívat v HAL_I2C_Mem_Read(.....,0xFFFFFFFF),
+//              pøi velice špatném naèasování vytažení tagu se tam program zasekne forever
 
-void bc_humidity_tag_init(bc_humidity_tag_t *self, bc_i2c_channel_t i2c_channel, bc_humidity_tag_i2c_address_t i2c_address)
-{
-    // TODO ... dala by se dodìlat autodetekce, ale z toho vyplívá dost problémù ...
-
-    if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HTS221)
-    {
-        if(i2c_address == BC_HUMIDITY_TAG_I2C_ADDRESS_DEFAULT)
-        {
-            bc_hts221_init((bc_hts221_t *)self, i2c_channel, BC_HTS221_I2C_ADDRESS_DEFAULT);
-        }
-        else
-        {
-            bc_hts221_init((bc_hts221_t *)self, i2c_channel, BC_HTS221_I2C_ADDRESS_ALTERNATE);
-        }
-    }
-    else if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HDC2080)
-    {
-        if(i2c_address == BC_HUMIDITY_TAG_I2C_ADDRESS_DEFAULT)
-        {
-            bc_hdc2080_init((bc_hdc2080_t *)self, i2c_channel, BC_HDC2080_I2C_ADDRESS_DEFAULT);
-        }
-        else
-        {
-            bc_hdc2080_init((bc_hdc2080_t *)self, i2c_channel, BC_HDC2080_I2C_ADDRESS_ALTERNATE);
-        }
-    }
-    else
-    {
-        for (;;);
-    }
-}
-
-void bc_humidity_tag_set_event_handler(bc_humidity_tag_t *self, void (*event_handler)(bc_humidity_tag_t *, bc_humidity_tag_event_t))
-{
-    if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HTS221)
-    {
-        bc_hts221_set_event_handler((bc_hts221_t *) &self->sensor, (void (*)(bc_hts221_t *, bc_hts221_event_t)) event_handler);
-    }
-    else if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HDC2080)
-    {
-        bc_hdc2080_set_event_handler((bc_hdc2080_t *) &self->sensor, (void (*)(bc_hdc2080_t *, bc_hdc2080_event_t)) event_handler);
-    }
-    else
-    {
-        for (;;);
-    }
-}
-
-void bc_humidity_tag_set_update_interval(bc_humidity_tag_t *self, bc_tick_t interval)
-{
-    if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HTS221)
-    {
-        bc_hts221_set_update_interval((bc_hts221_t *) &self->sensor, interval);
-    }
-    else if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HDC2080)
-    {
-        bc_hdc2080_set_update_interval((bc_hdc2080_t *) &self->sensor, interval);
-    }
-    else
-    {
-        for (;;);
-    }
-}
-
-bool bc_humidity_tag_get_humidity_raw(bc_humidity_tag_t *self, uint16_t *raw)
-{
-    if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HTS221)
-    {
-        return false;
-        // TODO ... not implemented
-        // return bc_hts221_get_humidity_raw((bc_hts221_t *) &self->device, raw);
-    }
-    else if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HDC2080)
-    {
-        return bc_hdc2080_get_humidity_raw((bc_hdc2080_t *) &self->sensor, raw);
-    }
-    else
-    {
-        for (;;);
-    }
-}
-
-bool bc_humidity_tag_get_humidity_percentage(bc_humidity_tag_t *self, float *percentage)
-{
-    if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HTS221)
-    {
-        return bc_hts221_get_humidity_percentage((bc_hts221_t *) &self->sensor, percentage);
-    }
-    else if (self->device_type == BC_HUMIDITY_TAG_DEVICE_HDC2080)
-    {
-        return bc_hdc2080_get_humidity_percentage((bc_hdc2080_t *) &self->sensor, percentage);
-    }
-    else
-    {
-        for (;;)
-            ;
-    }
-}
+void bc_humidity_tag_init(bc_humidity_tag_t *self, uint8_t i2c_channel, bc_humidity_tag_i2c_address_t i2c_address);
+void bc_humidity_tag_set_event_handler(bc_humidity_tag_t *self, void (*event_handler)(bc_humidity_tag_t *, bc_humidity_tag_event_t));
+void bc_humidity_tag_set_update_interval(bc_humidity_tag_t *self, bc_tick_t interval);
+bool bc_humidity_tag_get_humidity_raw(bc_humidity_tag_t *self, uint16_t *raw);
+bool bc_humidity_tag_get_humidity_percentage(bc_humidity_tag_t *self, float *percentage);
 
 #endif // _BC_HUMIDITY_TAG_H
