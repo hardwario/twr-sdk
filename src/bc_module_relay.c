7f1aef754d837e8d28f412e893c682474ed7e909
hubmartin <hub.martin@gmail.com>
2017-01-23 22:25:46 +0100

    Add relay_pulse and doc with example

--- src/bc_module_relay.c
+++ src/bc_module_relay.c
@@ -1,52 +1,109 @@
 #include <bc_module_power.h>
-//#include "stm32l0xx.h"
-
-//#include "usb_talk.h"
 #include <bc_scheduler.h>
 #include <bc_module_relay.h>
 #include <bc_scheduler.h>
 
 static bc_tick_t _bc_module_relay_task(void *param);
 
-// nepoužité výstupy do nuly
-// Init - měl by init přepnout relé do výchozího stavu??
+#define BC_MODULE_RELAY_I2C_ADDRESS_DEFAULT 0x3B
+#define BC_MODULE_RELAY_I2C_ADDRESS_ALTERNATE 0x3F
+
 void bc_module_relay_init(bc_module_relay_t *self)
 {
+    // Init instance
+    memset(self, 0, sizeof(*self));
+    // Init i2C expander driver
     bc_tca9534a_init(&self->_tca9534a, BC_I2C_I2C0, BC_MODULE_RELAY_I2C_ADDRESS_DEFAULT);
+    // Enable outputs
     bc_tca9534a_set_port_direction(&self->_tca9534a, 0x0F); // inverted: O = output
+    // Relay is bi-stable, in the begining we don't know the default state
+    self->_relay_state = BC_MODULE_RELAY_STATE_UNKNOWN;
+}
 
-    self->_timestamp = 0;
+static void bc_module_relay_scheduler_unregister(bc_module_relay_t *self)
+{
+    // Check if there is already a task running
+    if(self->_task_id)
+    {
+        // Unregister running task
+        bc_scheduler_unregister(self->_task_id);
+        // Clear task id
+        self->_task_id = 0;
+    }
+}
+
+static void bc_module_relay_scheduler_register(bc_module_relay_t *self)
+{
+    // Try unregister if we have a already running task
+    bc_module_relay_scheduler_unregister(self);
+
+    // Register relay task
+    self->_task_id = bc_scheduler_register(_bc_module_relay_task, self, bc_tick_get() + 20);
+
 }
 
 static bc_tick_t _bc_module_relay_task(void *param)
 {
     bc_module_relay_t *self = param;
 
-    // De-energize bistable relay coil - turn off
-    bc_tca9534a_write_port(&(self->_tca9534a), (1 << 6) | (1 << 4));
+    switch(self->_state)
+    {
+        // Final state turning off the coil
+        case BC_MODULE_RELAY_TASK_STATE_DEMAGNETIZE:
+            // De-energize bistable relay coil - turn off
+            bc_tca9534a_write_port(&(self->_tca9534a), (1 << 6) | (1 << 4));
+            // Unregister task
+            bc_module_relay_scheduler_unregister(self);
+            return 0;
+            break;
 
-    bc_scheduler_unregister(self->_task_id);
+        // Pulse mode - demagnetize the coil and set state to toggle the coil to other direction
+        case BC_MODULE_RELAY_TASK_STATE_DEMAGNETIZE_PULSE:
+            // De-energize bistable relay coil - turn off
+            bc_tca9534a_write_port(&(self->_tca9534a), (1 << 6) | (1 << 4));
+            // Set next task state
+            self->_state = BC_MODULE_RELAY_TASK_STATE_REVERSE_PULSE;
+            // Delay for set period of the pulse
+            return self->_pulse_length;
+            break;
+
+        // Pulse mode - toggle the relay to another direction
+        case BC_MODULE_RELAY_TASK_STATE_REVERSE_PULSE:
+            bc_module_relay_toggle(self);
+            return 20;
+            break;
+
+        default:
+            break;
+    }
 
     return 0;
 }
 
-// Tady to ještě pořešíme, protože na PCB je NC, NO ale vzpomínám si že jsme se bavili že došlo k nějaké lepší změně.
+
+
+
+
 void bc_module_relay_set(bc_module_relay_t *self, bc_module_relay_state_t state)
 {
     if(state)
     {
-        bc_tca9534a_write_port(&self->_tca9534a, (1 << 6) | (1 << 7)); // pol A
+        bc_tca9534a_write_port(&self->_tca9534a, (1 << 6) | (1 << 7)); // Polarity A
         self->_relay_state = BC_MODULE_RELAY_STATE_TRUE;
-        self->_task_id = bc_scheduler_register(_bc_module_relay_task, self, bc_tick_get() + 20);
     } else {
-        bc_tca9534a_write_port(&self->_tca9534a, (1 << 4) | (1 << 5)); // pol B
+        bc_tca9534a_write_port(&self->_tca9534a, (1 << 4) | (1 << 5)); // Polarity B
         self->_relay_state = BC_MODULE_RELAY_STATE_FALSE;
-        self->_task_id = bc_scheduler_register(_bc_module_relay_task, self, bc_tick_get() + 20);
     }
+
+    self->_state = BC_MODULE_RELAY_TASK_STATE_DEMAGNETIZE;
+    bc_module_relay_scheduler_register(self);
 }
+
+
 
 bool bc_module_relay_toggle(bc_module_relay_t *self)
 {
+
     if(self->_relay_state == BC_MODULE_RELAY_STATE_FALSE)
     {
         bc_module_relay_set(self, BC_MODULE_RELAY_STATE_TRUE);
@@ -62,9 +119,15 @@ bool bc_module_relay_toggle(bc_module_relay_t *self)
     return false;
 }
 
-//! Make a pulse
+
 void bc_module_relay_pulse(bc_module_relay_t *self, bc_tick_t duration)
 {
-    (void)self;
-    (void)duration;
+    // Save pulse duration
+    self->_pulse_length = duration;
+
+    // Set relay and schedule task
+    bc_module_relay_set(self, BC_MODULE_RELAY_STATE_TRUE);
+    // Manually change the state of the task to PULSE
+    self->_state = BC_MODULE_RELAY_TASK_STATE_DEMAGNETIZE_PULSE;
+
 }
