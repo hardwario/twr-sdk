0975523f27bca1b9c0255486fba254706fcb671d
hubmartin <hub.martin@gmail.com>
2017-01-29 19:44:23 +0100

    Add accelerometer alarm, don't work yet

--- inc/bc_lis2dh12.h
+++ inc/bc_lis2dh12.h
@@ -10,7 +10,8 @@
 typedef enum
 {
     BC_LIS2DH12_EVENT_ERROR = 0, //!< Error event
-    BC_LIS2DH12_EVENT_UPDATE = 1 //!< Update event
+    BC_LIS2DH12_EVENT_UPDATE = 1, //!< Update event
+    BC_LIS2DH12_EVENT_ALARM = 2 //!< Alarm event
 
 } bc_lis2dh12_event_t;
 
@@ -39,6 +40,19 @@ typedef struct
 
 } bc_lis2dh12_result_g_t;
 
+typedef struct
+{
+    float treshold;
+    uint8_t duration;
+    bool x_low;
+    bool x_high;
+    bool y_low;
+    bool y_high;
+    bool z_low;
+    bool z_high;
+
+} bc_lis2dh12_alarm_t;
+
 typedef struct bc_lis2dh12_t bc_lis2dh12_t;
 
 struct bc_lis2dh12_t
@@ -55,6 +69,7 @@ struct bc_lis2dh12_t
     uint8_t _out_y_h;
     uint8_t _out_z_l;
     uint8_t _out_z_h;
+    bool _alarm_active;
 };
 
 bool bc_lis2dh12_init(bc_lis2dh12_t *self, bc_i2c_channel_t i2c_channel, uint8_t i2c_address);
@@ -62,5 +77,10 @@ void bc_lis2dh12_set_event_handler(bc_lis2dh12_t *self, void (*event_handler)(bc
 void bc_lis2dh12_set_update_interval(bc_lis2dh12_t *self, bc_tick_t interval);
 bool bc_lis2dh12_get_result_raw(bc_lis2dh12_t *self, bc_lis2dh12_result_raw_t *result_raw);
 bool bc_lis2dh12_get_result_g(bc_lis2dh12_t *self, bc_lis2dh12_result_g_t *result_g);
+
+bool bc_lis2dh12_set_alarm(bc_lis2dh12_t *self, bc_lis2dh12_alarm_t *alarm);
+
+// Experimental testing
+//void bc_lis2dh12_set_config(bc_lis2dh12_t *self);
 
 #endif // _BC_LIS2DH12_H
--- src/bc_lis2dh12.c
+++ src/bc_lis2dh12.c
@@ -1,8 +1,8 @@
 #include <bc_lis2dh12.h>
 #include <bc_scheduler.h>
 
-#define BC_LIS2DH12_DELAY_RUN 50
-#define BC_LIS2DH12_DELAY_READ 50
+#define BC_LIS2DH12_DELAY_RUN 10
+#define BC_LIS2DH12_DELAY_READ 10
 
 static bc_tick_t _bc_lis2dh12_task(void *param, bc_tick_t tick_now);
 
@@ -136,9 +136,13 @@ start:
                 goto start;
             }
 
-            if (!_bc_lis2dh12_power_down(self))
+            // Power down only when no alarm is set
+            if(!self->_alarm_active)
             {
-                goto start;
+                if (!_bc_lis2dh12_power_down(self))
+                {
+                    goto start;
+                }
             }
 
             self->_accelerometer_valid = true;
@@ -152,6 +156,18 @@ start:
             if (self->_event_handler != NULL)
             {
                 self->_event_handler(self, BC_LIS2DH12_EVENT_UPDATE);
+
+                // Read Alarm bit
+                if(self->_alarm_active)
+                {
+                    uint8_t int1_src;
+                    bc_i2c_read_8b(self->_i2c_channel, self->_i2c_address, 0x39, &int1_src);
+
+                    if(int1_src & (1 << 6))
+                    {
+                        self->_event_handler(self, BC_LIS2DH12_EVENT_ALARM);
+                    }
+                }
             }
 
             self->_state = BC_LIS2DH12_STATE_MEASURE;
@@ -184,7 +200,8 @@ static bool _bc_lis2dh12_continuous_conversion(bc_lis2dh12_t *self)
         return false;
     }
 
-    if (!bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x20, 0x27))
+    // ODR = 0x5 => 100Hz
+    if (!bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x20, 0x57))
     {
         return false;
     }
@@ -237,3 +254,106 @@ static bool _bc_lis2dh12_read_result(bc_lis2dh12_t *self)
 
     return true;
 }
+
+bool bc_lis2dh12_set_alarm(bc_lis2dh12_t *self, bc_lis2dh12_alarm_t *alarm)
+{
+    if(alarm)
+    {
+        // Enable alarm
+        self->_alarm_active = true;
+
+        // Disable IRQ first to change the registers
+        /*if (!bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x30, 0x00))
+        {
+            return false;
+        }*/
+
+        // Recalculate threshold to the 4g full-scale setting
+        uint8_t int1_ths = (uint8_t)((alarm->treshold) / 0.016f);
+
+        if (!bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x32, int1_ths))
+        {
+            return false;
+        }
+
+        uint8_t int1_duration = (uint8_t)(alarm->duration / 1); // /10
+        if (!bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x33, int1_duration))
+        {
+            return false;
+        }
+
+        // INT_CFG1
+        uint8_t int_cfg1;
+
+        int_cfg1 = (0 << 7) | (1 << 6); // AOI = 0, 6D = 1
+
+        int_cfg1 |= (alarm->z_high) ? (1 << 5) : 0;
+        int_cfg1 |= (alarm->z_low) ? (1 << 4) : 0;
+
+        int_cfg1 |= (alarm->y_high) ? (1 << 3) : 0;
+        int_cfg1 |= (alarm->y_low) ? (1 << 2) : 0;
+
+        int_cfg1 |= (alarm->x_high) ? (1 << 1) : 0;
+        int_cfg1 |= (alarm->x_low) ? (1 << 0) : 0;
+
+        if (!bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x30, int_cfg1))
+        {
+            return false;
+        }
+
+        // ctr_reg5
+        /*uint8_t ctrl_reg5 = (1 << 3);
+        if (!bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x24, ctrl_reg5))
+        {
+            return false;
+        }*/
+    }
+    else
+    {
+        // Disable alarm
+        self->_alarm_active = false;
+
+        if (!bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x30, 0x00))
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+/*
+// ODR 100Hz 0x50, enable XYZ axes 0x07
+uint8_t ctrl_reg1 = 0x57;
+// No filters
+uint8_t ctrl_reg2 = 0x00;
+
+uint8_t click_cfg = 0x01; // X axis single click
+
+
+// 1 LSB = (full scale) / 128 => 2g/128 * 0,5 => 33
+uint8_t click_ths = 33;
+// 1 LSB = 1/ODR => 1/100Hz = 10ms ;  100ms/10ms = 10
+uint8_t time_limit = 10;
+
+// time latency to detect double-click, not used in single click
+uint8_t time_latency = 0;
+// Window for double click detection, not used in single click
+uint8_t time_window = 0;
+
+void bc_lis2dh12_set_config(bc_lis2dh12_t *self)
+{
+    bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x20, ctrl_reg1);
+    bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x21, ctrl_reg2);
+
+    bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x38, click_cfg);
+
+    //bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x39, click_src); // read only
+    bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x3A, click_ths);
+    bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x3B, time_limit);
+
+    bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x3C, time_latency);
+    bc_i2c_write_8b(self->_i2c_channel, self->_i2c_address, 0x3D, time_window);
+
+}
+*/
