cb98833e8e7ca33b91c31915f587d1427f2d66e5
mhubacek <mhubacek@cominfo.cz>
2017-02-01 10:59:24 +0100

    Add doxy comments

--- inc/bc_lis2dh12.h
+++ inc/bc_lis2dh12.h
@@ -4,8 +4,15 @@
 #include <bc_i2c.h>
 #include <bc_tick.h>
 
+
+//! @addtogroup bc_lis2dh12 bc_lis2dh12
+//! @brief Driver for LIS2DH12 accelerometer
+//! @{
+
 #define BC_LIS2DH12_ADDRESS_DEFAULT 0x18
 #define BC_LIS2DH12_ADDRESS_ALTERNATE 0x19
+
+//! @brief Callback events
 
 typedef enum
 {
@@ -25,6 +32,8 @@ typedef enum
 
 } bc_lis2dh12_state_t;
 
+//! @brief LIS2DH12 result in raw values
+
 typedef struct
 {
     int16_t x_axis;
@@ -33,6 +42,8 @@ typedef struct
 
 } bc_lis2dh12_result_raw_t;
 
+//! @brief LIS2DH12 result in g
+
 typedef struct
 {
     float x_axis;
@@ -40,6 +51,8 @@ typedef struct
     float z_axis;
 
 } bc_lis2dh12_result_g_t;
+
+//! @brief LIS2DH12 alarm set structure
 
 typedef struct
 {
@@ -54,7 +67,11 @@ typedef struct
 
 } bc_lis2dh12_alarm_t;
 
+//! @brief LIS2DH12 instance
+
 typedef struct bc_lis2dh12_t bc_lis2dh12_t;
+
+//! @brief LIS2DH12 instance
 
 struct bc_lis2dh12_t
 {
@@ -74,15 +91,53 @@ struct bc_lis2dh12_t
     bool _irq_flag;
 };
 
+//! @brief Initialize LIS2DH12
+//! @param[in] self Instance
+//! @param[in] i2c_channel I2C channel
+//! @param[in] i2c_address I2C device address
+
 bool bc_lis2dh12_init(bc_lis2dh12_t *self, bc_i2c_channel_t i2c_channel, uint8_t i2c_address);
+
+//! @brief Set callback function
+//! @param[in] self Instance
+//! @param[in] event_handler Function address
+
 void bc_lis2dh12_set_event_handler(bc_lis2dh12_t *self, void (*event_handler)(bc_lis2dh12_t *, bc_lis2dh12_event_t));
+
+//! @brief Set measurement interval
+//! @param[in] self Instance
+//! @param[in] interval Measurement interval
+
 void bc_lis2dh12_set_update_interval(bc_lis2dh12_t *self, bc_tick_t interval);
+
+//! @brief Get measured acceleration as raw value
+//! @param[in] self Instance
+//! @param[in] result_raw Pointer to structure where result will be stored
+//! @return true When value is valid
+//! @return false When value is invalid
+
 bool bc_lis2dh12_get_result_raw(bc_lis2dh12_t *self, bc_lis2dh12_result_raw_t *result_raw);
+
+//! @brief Get measured acceleration in g
+//! @param[in] self Instance
+//! @param[in] result_g Pointer to structure where result will be stored
+//! @return true When value is valid
+//! @return false When value is invalid
+
 bool bc_lis2dh12_get_result_g(bc_lis2dh12_t *self, bc_lis2dh12_result_g_t *result_g);
+
+//! @brief Enable or disable accelerometer threshold alarm
+//! @param[in] self Instance
+//! @param[in] alarm Pointer to structure with alarm configuration, if null then disable the alarm
+//! @return true When configuration was successful
+//! @return false When configuration was not successful
+
 bool bc_lis2dh12_set_alarm(bc_lis2dh12_t *self, bc_lis2dh12_alarm_t *alarm);
+
+//! @brief LIS1DH12 interrupt handler
+
 void bc_lis2dh12_signalize();
 
-// Experimental testing
-//void bc_lis2dh12_set_config(bc_lis2dh12_t *self);
+//! @}
 
 #endif // _BC_LIS2DH12_H
