e258fda63c097fc4c2b24348549c18079ecc0603
Petr Konvicka <petr.konvicka@bigclown.com>
2017-02-01 18:36:03 +0100

    Formalities

--- src/bc_dac.c
+++ src/bc_dac.c
@@ -1,16 +1,13 @@
 #include "bc_dac.h"
 #include "stm32l0xx.h"
 
-#define BC_DAC_PERIPHERAL_CLOCK_ENABLE() (RCC->APB1ENR |= RCC_APB1ENR_DACEN)
-#define BC_DAC_PERIPHERAL_CLOCK_DISABLE() (RCC->APB1ENR &= ~RCC_APB1ENR_DACEN)
-#define BC_DAC_GPIO_CLOCK_ENABLE(_PIN_) (RCC->IOPENR |= BC_PINS_GET_RCC_IOPENR_MASK(_PIN_))
-#define BC_DAC_CHANNEL0_SET(_CODE_) (DAC1->DHR12L1 = _CODE_)
-#define BC_DAC_CHANNEL1_SET(_CODE_) (DAC1->DHR12L2 = _CODE_)
+#define BC_DAC_CHANNEL_DAC0_SET(_CODE_) (DAC1->DHR12L1 = _CODE_)
+#define BC_DAC_CHANNEL_DAC1_SET(_CODE_) (DAC1->DHR12L2 = _CODE_)
 
-#define BC_DAC_VOLTAGE_TO_CODE_CONSTANT 0.0000502626747
+// Approximate voltage to code constant
+// #define BC_DAC_VOLTAGE_TO_CODE_CONSTANT 0.0000502626747
 
-// TODO ... velikosti channel cnt
-bc_dac_format_t bc_dac_setup[2];
+bc_dac_format_t bc_dac_setup[BC_DAC_CHANNEL_COUNT];
 
 void bc_dac_init(bc_dac_channel_t channel, bc_dac_format_t format)
 {
@@ -18,11 +15,10 @@ void bc_dac_init(bc_dac_channel_t channel, bc_dac_format_t format)
     bc_dac_setup[channel] = format;
 
     /* Enable peripheral code */
-    BC_DAC_PERIPHERAL_CLOCK_ENABLE();
+    RCC->APB1ENR |= RCC_APB1ENR_DACEN;
 
     /* Software trigger, disable output buffer, enable DACs */
-    DAC1->CR = DAC_CR_BOFF1 | DAC_CR_BOFF2 | DAC_CR_TSEL1 | DAC_CR_TSEL2
-            | DAC_CR_EN1 | DAC_CR_EN2;
+    DAC1->CR = DAC_CR_BOFF1 | DAC_CR_BOFF2 | DAC_CR_TSEL1 | DAC_CR_TSEL2 | DAC_CR_EN1 | DAC_CR_EN2;
 }
 
 void bc_dac_set_format(bc_dac_channel_t channel, bc_dac_format_t format)
@@ -36,12 +32,11 @@ bc_dac_format_t bc_dac_get_format(bc_dac_channel_t channel)
     return bc_dac_setup[channel];
 }
 
-// TODO .. pro4 se p5ed8v8 pointer<, hodnota bude rzchej39 o se t74e stacku i pr8ce s hodnotou
 void bc_dac_set_output(bc_dac_channel_t channel, const void *output)
 {
     uint16_t raw;
 
-    // TODO ... jednoduch7 p5evod, mo6n8 bude lep39 pou69t float a switch je na nic...
+    /* Handle input format */
     switch (bc_dac_setup[channel])
     {
     case BC_DAC_FORMAT_8_BIT:
@@ -57,20 +52,22 @@ void bc_dac_set_output(bc_dac_channel_t channel, const void *output)
         raw = ((*(uint32_t *) output >> 16) & 0xffff);
         break;
     default:
-        // TODO ... tadz bz to asi m2lo vr8ti chzbu
+        for (;;);
         break;
     }
 
+    /* Write raw on channel */
     switch (channel)
     {
-    case BC_DAC_DAC0:
-        BC_DAC_CHANNEL0_SET(raw);
+    case BC_DAC_CHANNEL_DAC0:
+        BC_DAC_CHANNEL_DAC0_SET(raw);
         break;
-    case BC_DAC_DAC1:
-        BC_DAC_CHANNEL1_SET(raw);
+    case BC_DAC_CHANNEL_DAC1:
+        BC_DAC_CHANNEL_DAC1_SET(raw);
         break;
+    case BC_DAC_CHANNEL_COUNT:
     default:
-        // TODO ... to sam7 tadz
+        for (;;);
         break;
     }
 }
