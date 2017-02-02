e55ece3b72465447e1d7769ba451f11ac3b3a5c8
hubmartin <hub.martin@gmail.com>
2017-02-02 19:47:26 +0100

    Add bc.h header file

---
+++ inc/bc.h
@@ -0,0 +1,26 @@
+#ifndef _BC_H
+#define _BC_H
+
+// #include <application.h>
+#include <bc_common.h>
+
+// CORE MODULE STUFF
+#include <usb_talk.h>
+#include <bc_led.h>
+#include <bc_button.h>
+#include <bc_lis2dh12.h>
+#include <bc_spirit1.h>
+#include <bc_gpio.h>
+#include <bc_i2c.h>
+
+// TAGS
+#include <bc_tag_temperature.h>
+#include <bc_tag_humidity.h>
+#include <bc_tag_lux_meter.h>
+#include <bc_tag_barometer.h>
+
+// MODULES
+#include <bc_module_relay.h>
+#include <bc_module_power.h>
+
+#endif /* _BC_H */
