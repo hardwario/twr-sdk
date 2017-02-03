5d383439dae6e97eb93c01f96b13888b012b1055
Petr Konvicka <petr.konvicka@bigclown.com>
2017-02-03 11:30:49 +0100

    Update pir wrapper, inline removed

--- inc/bc_module_pir.h
+++ inc/bc_module_pir.h
@@ -16,7 +16,7 @@ typedef bc_pyq1648_t bc_module_pir_t;
 //! @param[in] gpio_channel_setup PIR setup channel
 //! @param[in] gpio_channel_event PIR event channel
 
-inline void bc_module_pir_init(bc_module_pir_t *self)
+void bc_module_pir_init(bc_module_pir_t *self)
 {
     bc_pyq1648_init(self, BC_GPIO_P8, BC_GPIO_P9);
 }
@@ -25,7 +25,7 @@ inline void bc_module_pir_init(bc_module_pir_t *self)
 //! @param[in] self PIR image
 //! @param[in] event_handler PIR event handler
 
-inline void bc_module_pir_set_event_handler(bc_module_pir_t *self, void (*event_handler)(bc_module_pir_t *, bc_module_pir_event_t))
+void bc_module_pir_set_event_handler(bc_module_pir_t *self, void (*event_handler)(bc_module_pir_t *, bc_module_pir_event_t))
 {
     bc_pyq1648_set_event_handler(self, event_handler);
 }
@@ -34,7 +34,7 @@ inline void bc_module_pir_set_event_handler(bc_module_pir_t *self, void (*event_
 //! @param[in] self PIR module image
 //! @param[in] event_handler PIR module event handler
 
-inline void bc_module_pir_set_sensitivity(bc_pyq1648_t *self, bc_module_pir_sensitivity_t sensitivity)
+void bc_module_pir_set_sensitivity(bc_pyq1648_t *self, bc_module_pir_sensitivity_t sensitivity)
 {
     bc_pyq1648_set_sensitivity(self, sensitivity);
 }
