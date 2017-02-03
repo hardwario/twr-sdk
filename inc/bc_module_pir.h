7c5e04aeca34aac241aba6e4cd86d2a6fc616d06
Petr Konvicka <petr.konvicka@bigclown.com>
2017-02-03 12:51:10 +0100

    Update : spread pir to .c and .h

--- inc/bc_module_pir.h
+++ inc/bc_module_pir.h
@@ -16,29 +16,19 @@ typedef bc_pyq1648_t bc_module_pir_t;
 //! @param[in] gpio_channel_setup PIR setup channel
 //! @param[in] gpio_channel_event PIR event channel
 
-void bc_module_pir_init(bc_module_pir_t *self)
-{
-    bc_pyq1648_init(self, BC_GPIO_P9, BC_GPIO_P8);
-}
+void bc_module_pir_init(bc_module_pir_t *self);
 
 //! @brief Set PIR module event handler
 //! @param[in] self PIR image
 //! @param[in] event_handler PIR event handler
 
-void bc_module_pir_set_event_handler(bc_module_pir_t *self, void (*event_handler)(bc_module_pir_t *, bc_module_pir_event_t, void*), void *event_param)
-{
-    bc_pyq1648_set_event_handler(self, (void(*)(bc_pyq1648_t*, bc_pyq1648_event_t, void*))event_handler, event_param);
-}
+void bc_module_pir_set_event_handler(bc_module_pir_t *self, void (*event_handler)(bc_module_pir_t *, bc_module_pir_event_t, void*), void *event_param);
 
 //! @brief Set PIR module set sensitivity
 //! @param[in] self PIR module image
 //! @param[in] event_handler PIR module event handler
 
-void bc_module_pir_set_sensitivity(bc_pyq1648_t *self, bc_module_pir_sensitivity_t sensitivity)
-{
-    bc_pyq1648_set_sensitivity(self, sensitivity);
-}
-
+void bc_module_pir_set_sensitivity(bc_pyq1648_t *self, bc_module_pir_sensitivity_t sensitivity);
 //! @}
 
 #endif // _BC_MODULE_PIR_H
