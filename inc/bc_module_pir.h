7e91c3853031b28de80e288c93a6fddcef3fd587
Petr Konvicka <petr.konvicka@bigclown.com>
2017-02-03 11:29:50 +0100

    Fix pir wrapper, pins are now static

--- inc/bc_module_pir.h
+++ inc/bc_module_pir.h
@@ -11,24 +11,14 @@ typedef bc_pyq1648_sensitivity_t bc_module_pir_sensitivity_t;
 typedef bc_pyq1648_event_t bc_module_pir_event_t;
 typedef bc_pyq1648_t bc_module_pir_t;
 
-// TODO Delete this block...
-
-/*
- #define bc_module_pir_init(_SELF_, _GPIO_CHANNEL_SETUP_, _GPIO_CHANNEL_EVENT_) (bc_pyq1648_init((_SELF_), (_GPIO_CHANNEL_), (_GPIO_CHANNEL_EVENT_)))
- #define bc_module_pir_set_event_handler(_SELF_, _HANDLER_) (bc_pyq1648_set_event_handler((_SELF_), void (*event_handler)(_HANDLER_)))
- #define bc_module_pir_set_update_interval(_SELF_, _INTERVAL_) (bc_pyq1648_set_update_interval((_SELF_), (_INTERVAL_)));
- */
-
 //! @brief Initialize PIR module on channels gpio_channel_setup and gpio_channel_event
 //! @param[in] self PIR image
 //! @param[in] gpio_channel_setup PIR setup channel
 //! @param[in] gpio_channel_event PIR event channel
 
-// TODO Delete there should be no GPIO configuration - this is fixed by hardware
-
-inline void bc_module_pir_init(bc_module_pir_t *self, bc_gpio_channel_t gpio_channel_setup, bc_gpio_channel_t gpio_channel_event)
+inline void bc_module_pir_init(bc_module_pir_t *self)
 {
-    bc_pyq1648_init(self, gpio_channel_setup, gpio_channel_event);
+    bc_pyq1648_init(self, BC_GPIO_P8, BC_GPIO_P9);
 }
 
 //! @brief Set PIR module event handler
