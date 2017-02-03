9dcadb6a1fd7d70bb0374be9137c609916edb1e8
Petr Konvicka <petr.konvicka@bigclown.com>
2017-02-03 16:16:16 +0100

    JLink issue...

--- src/bc_spi.c
+++ src/bc_spi.c
@@ -1,6 +1,8 @@
 #include <bc_spi.h>
 #include <stm32l083xx.h>
 
+#include <stm32l0xx_hal.h>
+
 // cs ... p15
 // sclk ... p14
 // mosi ... p13
@@ -10,14 +12,38 @@ typedef struct
 {
     uint32_t CLKPolarity;
     uint32_t CLKPhase;
-} _bc_spi_mode_t;
+} _bc_spi_mode_t;   
 
-static _bc_spi_mode_t _bc_spi_mode_table[4];/* = {
-     [BC_SPI_MODE_0] = {SPI_POLARITY_LOW, SPI_PHASE_1EDGE },
-     [BC_SPI_MODE_1] = {SPI_POLARITY_LOW, SPI_PHASE_2EDGE },
-     [BC_SPI_MODE_2] = {SPI_POLARITY_HIGH, SPI_PHASE_1EDGE },
-     [BC_SPI_MODE_3] = {SPI_POLARITY_HIGH, SPI_PHASE_2EDGE }
-}; */
+
+/*
+    BC_SPI_SPEED_1_MHZ = 0L, //!< SPI communication speed is 1 MHz
+    BC_SPI_SPEED_2_MHZ = 1L, //!< SPI communication speed is 2 MHz
+    BC_SPI_SPEED_4_MHZ = 2L, //!< SPI communication speed is 4 MHz
+    BC_SPI_SPEED_8_MHZ = 3L  //!< SPI communication speed is 8 MHz
+*/
+/*
+static uint32_t _bc_spi_speed_table[4] = {
+    [0] = SPI_CR1_BR_1 | SPI_CR1_BR_0,  // :16
+    [1] = SPI_CR1_BR_1,                 // :8
+    [2] = SPI_CR1_BR_0,                 // :4
+    [3] = 0x00,                         // :2
+};
+*/
+
+static uint32_t _bc_spi_speed_table[4] = {
+    [BC_SPI_SPEED_1_MHZ] = 0x18, // :16 (2MHz)
+    [BC_SPI_SPEED_2_MHZ] = 0x10, // :8 (4MHz)
+    [BC_SPI_SPEED_4_MHZ] = 0x08, // :4 (8MHz)
+    [BC_SPI_SPEED_8_MHZ] = 0x00, // :2 (16MHz)
+};
+
+// TODO ... dirty
+static uint32_t _bc_spi_mode_table[4] = {
+    [BC_SPI_MODE_0] = 0x00 ,
+    [BC_SPI_MODE_1] = 0x01 ,
+    [BC_SPI_MODE_2] = 0x02 ,
+    [BC_SPI_MODE_3] = 0x03
+};
 
 /* Stocked API related variables */
 static bc_spi_mode_t _bc_spi_mode;
@@ -27,6 +53,7 @@ static void _bc_spi_init_gpio();
 static void _bc_spi_init_spi(bc_spi_speed_t speed, bc_spi_mode_t mode);
 static void _bc_spi_set_speed(bc_spi_speed_t speed);
 static void _bc_spi_set_mode(bc_spi_mode_t mode);
+static uint8_t _bc_spi_transfer_byte(uint8_t value);
 
 void bc_spi_init(bc_spi_speed_t speed, bc_spi_mode_t mode)
 {
@@ -59,7 +86,30 @@ void bc_spi_transfer(const void *source, void *destination, size_t length)
 
 }
 
+static uint8_t _bc_spi_transfer_byte(uint8_t value)
+{
+    // TODO ... future trouble
 
+    // Wait until transmit buffer is empty
+    while ((SPI1->SR & SPI_SR_TXE) == 0)
+    {
+        continue;
+    }
+
+    // Write data register
+    SPI1->DR = value;
+
+    // Wait until receive buffer is full
+    while ((SPI1->SR & SPI_SR_RXNE) == 0)
+    {
+        continue;
+    }
+
+    // Read data register
+    value = SPI1->DR;
+
+    return value;
+}
 
 static void _bc_spi_init_gpio()
 {
@@ -101,14 +151,30 @@ static void _bc_spi_init_spi(bc_spi_speed_t speed, bc_spi_mode_t mode)
 
 static void _bc_spi_set_speed(bc_spi_speed_t speed)
 {
-    // TODO ... add clock switch
+    uint32_t temp;
+
+    // Store speed to private variable
     _bc_spi_speed = speed;
+
+    // Edit baud rate divider
+    temp = SPI2->CR1;
+    temp &= ~SPI_CR1_BR_Msk;
+    temp |= _bc_spi_speed_table[speed];
+    SPI2->CR1 = temp;
 }
 
 static void _bc_spi_set_mode(bc_spi_mode_t mode)
 {
-    // TODO ... add mode switch
+    uint32_t temp;
+
+    // Store mode to private variable
     _bc_spi_mode = mode;
+
+    // Edit mode
+    temp = SPI2->CR1;
+    SPI2->CR1 &= ~0x03;
+    SPI2->CR1 |= _bc_spi_mode_table[mode];
+    SPI2->CR1 = temp;
 }
 
 
