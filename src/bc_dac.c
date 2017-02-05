ff5a3284c2ff6936eb4e26126e201bb43f3d2478
Pavel Hübner <pavel.hubner@bigclown.com>
2017-02-05 10:46:57 +0100

    bc_dac: Refactor and improve documentation

--- src/bc_dac.c
+++ src/bc_dac.c
@@ -1,59 +1,60 @@
-#include "bc_dac.h"
-#include "stm32l0xx.h"
+#include <bc_dac.h>
+#include <stm32l0xx.h>
 
 // Approximate voltage to code constant
-#define BC_DAC_VOLTAGE_TO_CODE_CONSTANT 19961;
+#define _BC_DAC_VOLTAGE_TO_CODE_CONSTANT 19961
 
-static bc_dac_format_t _bc_dac_setup[2];
+static bc_dac_format_t _bc_dac_format[2];
 
 void bc_dac_init(bc_dac_channel_t channel, bc_dac_format_t format)
 {
-    // Store channel input format
-    _bc_dac_setup[channel] = format;
+    // Store input format of raw value
+    _bc_dac_format[channel] = format;
 
-    // Enable peripheral code
+    // Enable DAC clock
     RCC->APB1ENR |= RCC_APB1ENR_DACEN;
 
     // Software trigger, disable output buffer, enable DACs
-    DAC1->CR = DAC_CR_BOFF1 | DAC_CR_BOFF2 | DAC_CR_TSEL1 | DAC_CR_TSEL2 | DAC_CR_EN1 | DAC_CR_EN2;
+    DAC1->CR |= DAC_CR_TSEL2_2 | DAC_CR_TSEL2_1 | DAC_CR_TSEL2_0 | DAC_CR_BOFF2 | DAC_CR_EN2;
+    DAC1->CR |= DAC_CR_TSEL1_2 | DAC_CR_TSEL1_1 | DAC_CR_TSEL1_0 | DAC_CR_BOFF1 | DAC_CR_EN1;
 }
 
 void bc_dac_set_format(bc_dac_channel_t channel, bc_dac_format_t format)
 {
     // Store channel input format
-    _bc_dac_setup[channel] = format;
+    _bc_dac_format[channel] = format;
 }
 
 bc_dac_format_t bc_dac_get_format(bc_dac_channel_t channel)
 {
-    return _bc_dac_setup[channel];
+    return _bc_dac_format[channel];
 }
 
-void bc_dac_set_output_raw(bc_dac_channel_t channel, const void *output)
+void bc_dac_set_output_raw(bc_dac_channel_t channel, const void *raw)
 {
-    uint16_t raw;
+    uint16_t dac_value;
 
-    // Handle input format
-    switch (_bc_dac_setup[channel])
+    // Convert input value in configured format to raw value
+    switch (_bc_dac_format[channel])
     {
         case BC_DAC_FORMAT_8_BIT:
         {
-            raw = *(uint16_t *) output << 8;
+            dac_value = *(uint16_t *) raw << 8;
             break;
         }
         case BC_DAC_FORMAT_16_BIT:
         {
-            raw = *(uint16_t *) output;
+            dac_value = *(uint16_t *) raw;
             break;
         }
         case BC_DAC_FORMAT_24_BIT:
         {
-            raw = *(uint32_t *) output >> 8;
+            dac_value = *(uint32_t *) raw >> 8;
             break;
         }
         case BC_DAC_FORMAT_32_BIT:
         {
-            raw = *(uint32_t *) output >> 16;
+            dac_value = *(uint32_t *) raw >> 16;
             break;
         }
         default:
@@ -62,17 +63,17 @@ void bc_dac_set_output_raw(bc_dac_channel_t channel, const void *output)
         }
     }
 
-    // Write raw on channel
+    // Write raw value to DAC channel
     switch (channel)
     {
-        case BC_DAC_CHANNEL_DAC0:
+        case BC_DAC_DAC0:
         {
-            DAC1->DHR12L1 = raw;
+            DAC1->DHR12L1 = dac_value;
             break;
         }
-        case BC_DAC_CHANNEL_DAC1:
+        case BC_DAC_DAC1:
         {
-            DAC1->DHR12L2 = raw;
+            DAC1->DHR12L2 = dac_value;
             break;
         }
         default:
@@ -84,18 +85,18 @@ void bc_dac_set_output_raw(bc_dac_channel_t channel, const void *output)
 
 void bc_dac_set_output_voltage(bc_dac_channel_t channel, float voltage)
 {
-    // Convert voltage to DAC code
-    uint16_t raw = voltage * BC_DAC_VOLTAGE_TO_CODE_CONSTANT;
+    // Convert desired voltage to raw DAC code
+    uint16_t raw = voltage * _BC_DAC_VOLTAGE_TO_CODE_CONSTANT;
 
-    // Write raw on channel
+    // Write raw value to DAC channel
     switch (channel)
     {
-        case BC_DAC_CHANNEL_DAC0:
+        case BC_DAC_DAC0:
         {
             DAC1->DHR12L1 = raw;
             break;
         }
-        case BC_DAC_CHANNEL_DAC1:
+        case BC_DAC_DAC1:
         {
             DAC1->DHR12L2 = raw;
             break;
