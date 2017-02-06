6a7804421ae6d0f59e6fea434ccbef7476acf77d
Petr Konvicka <petr.konvicka@bigclown.com>
2017-02-06 15:02:51 +0100

    Update format handling

--- src/bc_adc.c
+++ src/bc_adc.c
@@ -2,58 +2,45 @@
 #include <bc_adc.h>
 #include <stm32l083xx.h>
 
-typedef struct {
+typedef struct
+{
     bc_adc_reference_t reference;
     bc_adc_format_t format;
-    uint32_t chsel;
+    uint32_t chselr;
 } bc_adc_config_t;
 
-static bc_adc_config_t _bc_adc_config_table[6] = {
-    [0].chsel = ADC_CHSELR_CHSEL0,
-    [1].chsel = ADC_CHSELR_CHSEL1,
-    [2].chsel = ADC_CHSELR_CHSEL2,
-    [3].chsel = ADC_CHSELR_CHSEL3,
-    [4].chsel = ADC_CHSELR_CHSEL4,
-    [5].chsel = ADC_CHSELR_CHSEL5
-};
+static bc_adc_config_t _bc_adc_config_table[6] =
+{
+    [0].chselr = ADC_CHSELR_CHSEL0,
+    [1].chselr = ADC_CHSELR_CHSEL1,
+    [2].chselr = ADC_CHSELR_CHSEL2,
+    [3].chselr = ADC_CHSELR_CHSEL3,
+    [4].chselr = ADC_CHSELR_CHSEL4,
+    [5].chselr = ADC_CHSELR_CHSEL5 };
 
 void bc_adc_init(bc_adc_channel_t channel, bc_adc_reference_t reference, bc_adc_format_t format)
 {
     RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // Enable ADC peripheral clock
 
-    // Clear the ADRDY bit
-    ADC1->ISR |= ADC_ISR_ADRDY;
-
-    ADC1->CR &= (uint32_t) (~ADC_CR_ADEN); // Clear ADEN
-    ADC1->CR |= ADC_CR_ADCAL; // Start calibration
-
-    // Wait untill calibration is done
-    while ((ADC1->ISR & ADC_ISR_EOCAL) == 0) /* (4) */
+    // ADC calibration
+    ADC1->CR &= (uint32_t) (~ADC_CR_ADEN);  // Clear ADEN
+    ADC1->CR |= ADC_CR_ADCAL;               // Start calibration
+    while ((ADC1->ISR & ADC_ISR_EOCAL) == 0)
     {
-        /* For robust implementation, add here time-out management */
+        continue;                           // Wait untill calibration is done
     }
+    ADC1->ISR |= ADC_ISR_EOCAL;             // Clear EOCAL
 
-    // Clear EOCAL
-    ADC1->ISR |= ADC_ISR_EOCAL;
-
-    // Enable the ADC
-    ADC1->CR |= ADC_CR_ADEN;
-
-    // Select the auto off mode
-    ADC1->CFGR1 |= ADC_CFGR1_AUTOFF;
-
-    // Wait until ADC ready
-    if ((ADC1->CFGR1 & ADC_CFGR1_AUTOFF) == 0)
-    {
-        while ((ADC1->ISR & ADC_ISR_ADRDY) == 0)
-        {
-            /* For robust implementation, add here time-out management */
-        }
-    }
-
+    // Enable and configure ADC
+    ADC1->ISR |= ADC_ISR_ADRDY;                         // Clear the ADRDY bit
+    ADC1->CR |= ADC_CR_ADEN;                            // Enable the ADC
+    ADC1->CFGR1 |= ADC_CFGR1_AUTOFF |                   // Select the auto off mode
+                   ADC_CFGR1_ALIGN;                     // Left alignment
     ADC1->CFGR2 = (ADC1->CFGR2 & (~ADC_CFGR2_CKMODE)) | // Asynchronous clock mode
-                  (ADC_CFGR2_OVSE |   // Oversampler Enable
-                   ADC_CFGR2_OVSR_1 | ADC_CFGR2_OVSR_0); // Oversampling ratio (16x)
+    (ADC_CFGR2_OVSE |                                   // Oversampler Enable
+    ADC_CFGR2_OVSR_1 | ADC_CFGR2_OVSR_0);               // Oversampling ratio (16x)
+
+    ADC1->SMPR |= ADC_SMPR_SMP_0 | ADC_SMPR_SMP_1 | ADC_SMPR_SMP_2; // Select a sampling mode
 
     // Store desired reference
     bc_adc_set_reference(channel, reference);
@@ -90,11 +77,10 @@ bc_adc_format_t bc_adc_get_format(bc_adc_channel_t channel)
 
 void bc_adc_measure(bc_adc_channel_t channel, void *result)
 {
-    // Set adc channel
-    ADC1->CHSELR = _bc_adc_config_table[channel].chsel; // Select CHSEL17 for VRefInt
-    
-    ADC1->SMPR |= ADC_SMPR_SMP_0 | ADC_SMPR_SMP_1 | ADC_SMPR_SMP_2; // Select a sampling mode
-    // ADC->CCR |= ADC_CCR_VREFEN; // Wake-up the VREFINT (only for Temp sensor and VRefInt)
+    uint32_t data;
+
+    // Set ADC channel
+    ADC1->CHSELR = _bc_adc_config_table[channel].chselr; // Select CHSEL17 for VRefInt
 
     // Clear EOC, EOS and OVR flags
     ADC1->ISR = (ADC_ISR_EOS | ADC_ISR_OVR);
@@ -102,15 +88,33 @@ void bc_adc_measure(bc_adc_channel_t channel, void *result)
     // Performs the AD conversion
     ADC1->CR |= ADC_CR_ADSTART;
 
-    // wait end of sequence
+    // wait for end of sequence
     while ((ADC1->ISR & ADC_ISR_EOS) == 0)
     {
-    /* For robust implementation, add here time-out management */
+        continue;
     }
+
+    data = ADC1->DR;
 
     // TODO ... take care of reference ...
 
-    // TODO ... take care of format ...
-
-    // readout data in format
+    // TODO ... maybe float is better
+    switch (_bc_adc_config_table[channel].format)
+    {
+    case BC_ADC_FORMAT_8_BIT:
+        *(uint8_t *)result = data >> 8;
+        break;
+    case BC_ADC_FORMAT_16_BIT:
+        *(uint16_t *)result = data;
+        break;
+    case BC_ADC_FORMAT_24_BIT:
+        memcpy((uint8_t *)result + 1, &data, 2);
+        break;
+    case BC_ADC_FORMAT_32_BIT:
+        *(uint32_t *)result = data << 16;
+        break;
+    default:
+        return;
+        break;
+    }
 }
