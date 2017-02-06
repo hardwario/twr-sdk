9419e6a1cfd0ba31a8581746aba9bd48b12e2dcd
Petr Konvicka <petr.konvicka@bigclown.com>
2017-02-06 11:28:38 +0100

    fix baud rate prescaler table

--- src/bc_spi.c
+++ src/bc_spi.c
@@ -16,10 +16,10 @@ typedef struct
 
 static uint32_t _bc_spi_speed_table[4] =
 {
-    [BC_SPI_SPEED_1_MHZ] = 0x18, // :16 (2MHz)
-    [BC_SPI_SPEED_2_MHZ] = 0x10, // :8 (4MHz)
-    [BC_SPI_SPEED_4_MHZ] = 0x08, // :4 (8MHz)
-    [BC_SPI_SPEED_8_MHZ] = 0x00, // :2 (16MHz)
+    [BC_SPI_SPEED_1_MHZ] = 0x20, // :16 (1MHz)
+    [BC_SPI_SPEED_2_MHZ] = 0x18, // :8 (2MHz)
+    [BC_SPI_SPEED_4_MHZ] = 0x10, // :4 (4MHz)
+    [BC_SPI_SPEED_8_MHZ] = 0x08, // :2 (8MHz)
 };
 
 static uint32_t _bc_spi_mode_table[4] =
