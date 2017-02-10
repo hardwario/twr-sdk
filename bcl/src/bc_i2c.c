e65d1b83a5a6834cacd95c3d2869a32452e33796
Petr Konvicka <petr.konvicka@bigclown.com>
2017-02-10 16:40:57 +0100

    Set clocks to 16MHz(HSI16) + fix basic peripherals(bc_spi, bc_i2c and bc_uart)

--- bcl/src/bc_i2c.c
+++ bcl/src/bc_i2c.c
@@ -42,7 +42,8 @@ void bc_i2c_init(bc_i2c_channel_t channel, bc_i2c_speed_t speed)
         __HAL_RCC_I2C2_CLK_ENABLE();
 
         bc_i2c.handle_i2c0.Instance = I2C2;
-        bc_i2c.handle_i2c0.Init.Timing = 0x00707CBB;
+        // bc_i2c.handle_i2c0.Init.Timing = 0x00707CBB;
+        bc_i2c.handle_i2c0.Init.Timing = 0x0010061A;
         bc_i2c.handle_i2c0.Init.OwnAddress1 = 0;
         bc_i2c.handle_i2c0.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
         bc_i2c.handle_i2c0.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
@@ -85,7 +86,8 @@ void bc_i2c_init(bc_i2c_channel_t channel, bc_i2c_speed_t speed)
         __HAL_RCC_I2C1_CLK_ENABLE();
 
         bc_i2c.handle_i2c1.Instance = I2C1;
-        bc_i2c.handle_i2c1.Init.Timing = 0x00707CBB;
+        // bc_i2c.handle_i2c1.Init.Timing = 0x00707CBB;
+        bc_i2c.handle_i2c0.Init.Timing = 0x0010061A;
         bc_i2c.handle_i2c1.Init.OwnAddress1 = 0;
         bc_i2c.handle_i2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
         bc_i2c.handle_i2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
