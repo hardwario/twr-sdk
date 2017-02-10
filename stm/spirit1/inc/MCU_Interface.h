c1dbacc8e3c24adb06bc577c2e580b9c16779e04
Pavel Hübner <pavel.hubner@bigclown.com>
2017-02-10 11:42:46 +0100

    Add "stm" sub-directory (formerly "bc-module-core-stm" submodule)

---
+++ stm/spirit1/inc/MCU_Interface.h
@@ -0,0 +1,155 @@
+/**
+ * @file    MCU_Interface.h
+ * @author  AMG - RF Application team
+ * @version 3.2.4
+ * @date    26-September-2016
+ * @brief   Interface for the low level SPIRIT SPI driver.
+ * @details
+ * This header file constitutes an interface to the SPI driver used to
+ * communicate with Spirit.
+ * It exports some function prototypes to write/read registers and FIFOs
+ * and to send command strobes.
+ * Since the Spirit libraries are totally platform independent, the implementation
+ * of these functions are not provided here. The user have to implement these functions
+ * taking care to keep the exported prototypes.
+ *
+ * These functions are:
+ *
+ * <ul>
+ * <li>SdkEvalSpiDeinit</li>
+ * <li>SpiritSpiInit</li>
+ * <li>SpiritSpiWriteRegisters</li>
+ * <li>SpiritSpiReadRegisters</li>
+ * <li>SpiritSpiCommandStrobes</li>
+ * <li>SpiritSpiWriteLinearFifo</li>
+ * <li>SpiritSpiReadLinearFifo</li>
+ * </ul>
+ *
+ * @note An example of SPI driver implementation is available in the <i>Sdk_Eval</i> library.
+ *
+ * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+ * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+ * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+ * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+ * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+ * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+ *
+ * THIS SOURCE CODE IS PROTECTED BY A LICENSE.
+ * FOR MORE INFORMATION PLEASE CAREFULLY READ THE LICENSE AGREEMENT FILE LOCATED
+ * IN THE ROOT DIRECTORY OF THIS FIRMWARE PACKAGE.
+ *
+ * <h2><center>&copy; COPYRIGHT 2016 STMicroelectronics</center></h2>
+ */
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MCU_INTERFACE_H
+#define __MCU_INTERFACE_H
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "SPIRIT_Types.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/** @addtogroup SPIRIT_Libraries
+ * @{
+ */
+
+
+/** @defgroup SPIRIT_SPI_Driver         SPI Driver
+ * @brief Header file for low level SPIRIT SPI driver.
+ * @details See the file <i>@ref MCU_Interface.h</i> for more details.
+ * @{
+ */
+
+
+
+/** @defgroup SPI_Exported_Types        SPI Exported Types
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+
+
+/** @defgroup SPI_Exported_Constants    SPI Exported Constants
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+
+
+/** @defgroup SPI_Exported_Macros       SPI Exported Macros
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+
+
+/** @defgroup SPI_Exported_Functions    SPI Exported Functions
+ * @{
+ */
+
+typedef SpiritStatus StatusBytes;
+
+void SdkEvalSpiDeinit(void);
+void SdkEvalSpiInit(void);
+StatusBytes SdkEvalSpiWriteRegisters(uint8_t cRegAddress, uint8_t cNbBytes, uint8_t* pcBuffer);
+StatusBytes SdkEvalSpiReadRegisters(uint8_t cRegAddress, uint8_t cNbBytes, uint8_t* pcBuffer);
+StatusBytes SdkEvalSpiCommandStrobes(uint8_t cCommandCode);
+StatusBytes SdkEvalSpiWriteFifo(uint8_t cNbBytes, uint8_t* pcBuffer);
+StatusBytes SdkEvalSpiReadFifo(uint8_t cNbBytes, uint8_t* pcBuffer);
+
+#define bc_spirit_status_t SpiritStatus
+
+bc_spirit_status_t bc_spirit1_command(uint8_t command);
+bc_spirit_status_t bc_spirit1_write(uint8_t address, const void *buffer, size_t length);
+bc_spirit_status_t bc_spirit1_read(uint8_t address, void *buffer, size_t length);
+void bc_spirit1_hal_init(void);
+void bc_spirit1_hal_shutdown_low(void);
+void bc_spirit1_hal_shutdown_high(void);
+
+#define SpiritEnterShutdown                                            bc_spirit1_hal_shutdown_high
+#define SpiritExitShutdown                                             bc_spirit1_hal_shutdown_low
+#define SpiritSpiInit                                                  bc_spirit1_hal_init
+#define SpiritSpiWriteRegisters(cRegAddress, cNbBytes, pcBuffer)       bc_spirit1_write(cRegAddress, pcBuffer, cNbBytes)
+#define SpiritSpiReadRegisters(cRegAddress, cNbBytes, pcBuffer)        bc_spirit1_read(cRegAddress, pcBuffer, cNbBytes)
+#define SpiritSpiCommandStrobes(cCommandCode)                          bc_spirit1_command(cCommandCode)
+#define SpiritSpiWriteLinearFifo(cNbBytes, pcBuffer)                   bc_spirit1_write(0xFF, pcBuffer, cNbBytes)
+#define SpiritSpiReadLinearFifo(cNbBytes, pcBuffer)                    bc_spirit1_read(0xFF, pcBuffer, cNbBytes)
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+
+/**
+ * @}
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/******************* (C) COPYRIGHT 2016 STMicroelectronics *****END OF FILE****/
---
+++ stm/spirit1/inc/SDK_Configuration_Common.h
@@ -0,0 +1,187 @@
+/**
+* @file    SDK_BasicPktTest_Common.h
+* @author  VMA division - AMS
+* @version 3.2.1
+* @date    May 1, 2015
+* @brief   Common configuration header file.
+* @details
+*
+* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+* TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+* DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+* FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+* CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*
+* THIS SOURCE CODE IS PROTECTED BY A LICENSE.
+* FOR MORE INFORMATION PLEASE CAREFULLY READ THE LICENSE AGREEMENT FILE LOCATED
+* IN THE ROOT DIRECTORY OF THIS FIRMWARE PACKAGE.
+*
+* <h2><center>&copy; COPYRIGHT 2015 STMicroelectronics</center></h2>
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SDK_PKT_TEST_COMMON_H
+#define __SDK_PKT_TEST_COMMON_H
+
+#define USE_HIGH_BAND
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/**
+ * @addtogroup SDK_Examples
+ * @{
+ */
+
+/**
+ * @defgroup SDK_BasicPktTest
+ * @{
+ */
+
+/**
+ * @defgroup SDK_BasicPktTest_Common                                    SDK Basic Pkt Test Common
+ * @brief Radio and packet parameters definitions.
+ * @details These parameters are in common between the device A and B.
+ *
+ * The user can change the configuration parameters editing these defines.
+ * @{
+ */
+
+/**
+ * @defgroup BasicPktTest_Common_Exported_Types                         Basic Pkt Test Common Exported Types
+ * @{
+ */
+
+/**
+ *@}
+ */
+
+
+/**
+ * @defgroup BasicPktTest_Common_Exported_Constants                     Basic Pkt Test Common Exported Constants
+ * @{
+ */
+
+
+/*  Radio configuration parameters  */
+#define XTAL_OFFSET_PPM             0
+
+#ifdef USE_VERY_LOW_BAND
+#define BASE_FREQUENCY              169.0e6
+#endif
+
+#ifdef USE_LOW_BAND
+#define BASE_FREQUENCY              315.0e6
+#endif
+
+#ifdef USE_MIDDLE_BAND
+#define BASE_FREQUENCY              433.0e6
+#endif
+
+#ifdef USE_HIGH_BAND
+#define BASE_FREQUENCY              868.0e6
+#endif
+
+#define CHANNEL_SPACE               20e3
+#define CHANNEL_NUMBER              0
+#define MODULATION_SELECT           GFSK_BT1
+#define DATARATE                    19200
+#define FREQ_DEVIATION              20e3
+#define BANDWIDTH                   100E3
+
+#define POWER_DBM                   11.6 /* max is 11.6 */
+
+#define RSSI_THRESHOLD              0x20
+
+/*  Packet configuration parameters  */
+#define PREAMBLE_LENGTH             PKT_PREAMBLE_LENGTH_04BYTES
+#define SYNC_LENGTH                 PKT_SYNC_LENGTH_4BYTES
+#define SYNC_WORD                   0x88888888
+#define LENGTH_TYPE                 PKT_LENGTH_VAR
+#define LENGTH_WIDTH                8
+#define CRC_MODE                    PKT_CRC_MODE_8BITS
+#define CONTROL_LENGTH              PKT_CONTROL_LENGTH_0BYTES
+#define EN_ADDRESS                  S_DISABLE
+#define EN_FEC                      S_DISABLE
+#define EN_WHITENING                S_ENABLE
+
+/*  Addresses configuration parameters  */
+#define EN_FILT_MY_ADDRESS          S_DISABLE
+#define MY_ADDRESS                  0x34
+#define EN_FILT_MULTICAST_ADDRESS   S_DISABLE
+#define MULTICAST_ADDRESS           0xEE
+#define EN_FILT_BROADCAST_ADDRESS   S_DISABLE
+#define BROADCAST_ADDRESS           0xFF
+#define DESTINATION_ADDRESS         0x44
+
+/* Wake Up timer in ms for LDC mode */
+#define WAKEUP_TIMER                100.0
+/**
+ *@}
+ */
+
+
+/**
+ * @defgroup BasicPktTest_Common_Exported_Macros                                Basic Pkt Test Common Exported Macros
+ * @{
+   */
+
+#ifdef STM8L
+/* For STM8L we don't have the SPIRIT_SDK_Util module, so use this macro to set the RANGE EXT config */
+typedef enum
+{
+  RANGE_EXT_NONE = 0x00,
+  RANGE_EXT_SKYWORKS_169,
+  RANGE_EXT_SKYWORKS_868
+} RangeExtType;
+
+
+#define RANGE_EXT_INIT(RANGE_TYPE)        {\
+   if(RANGE_TYPE==RANGE_EXT_SKYWORKS_169) {\
+    SpiritGeneralSetExtRef(MODE_EXT_XIN);\
+    uint8_t tmp = 0x01; SpiritSpiWriteRegisters(0xB6,1,&tmp);\
+    SpiritGpioInit(&(SGpioInit){SPIRIT_GPIO_0, SPIRIT_GPIO_MODE_DIGITAL_OUTPUT_HP, SPIRIT_GPIO_DIG_OUT_TX_RX_MODE});\
+    SpiritGpioInit(&(SGpioInit){SPIRIT_GPIO_1, SPIRIT_GPIO_MODE_DIGITAL_OUTPUT_HP, SPIRIT_GPIO_DIG_OUT_TX_STATE});\
+    SpiritGpioInit(&(SGpioInit){SPIRIT_GPIO_2, SPIRIT_GPIO_MODE_DIGITAL_OUTPUT_HP, SPIRIT_GPIO_DIG_OUT_RX_STATE});\
+  }else if(RANGE_TYPE==RANGE_EXT_SKYWORKS_868){\
+    SpiritGpioInit(&(SGpioInit){SPIRIT_GPIO_0, SPIRIT_GPIO_MODE_DIGITAL_OUTPUT_HP, SPIRIT_GPIO_DIG_OUT_TX_RX_MODE});\
+    SpiritGpioInit(&(SGpioInit){SPIRIT_GPIO_1, SPIRIT_GPIO_MODE_DIGITAL_OUTPUT_HP, SPIRIT_GPIO_DIG_OUT_RX_STATE});\
+    SpiritGpioInit(&(SGpioInit){SPIRIT_GPIO_2, SPIRIT_GPIO_MODE_DIGITAL_OUTPUT_HP, SPIRIT_GPIO_DIG_OUT_TX_STATE});\
+  }\
+ }
+#endif
+/**
+ *@}
+ */
+
+
+/**
+ * @defgroup BasicPktTest_Common_Exported_Functions                             Basic Pkt Test Common Exported Functions
+ * @{
+ */
+
+/**
+ *@}
+ */
+
+/**
+ *@}
+ */
+
+/**
+ *@}
+ */
+
+/**
+ *@}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/******************* (C) COPYRIGHT 2015 STMicroelectronics *****END OF FILE****/
