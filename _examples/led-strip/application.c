1ab0f77e107c2a111fd40f864d93157e3b153496
Karel Blavka <karel.blavka@bigclown.com>
2017-02-21 23:27:55 +0100

    Add event effect done for led strip

--- _examples/led-strip/application.c
+++ _examples/led-strip/application.c
@@ -3,7 +3,8 @@
 
 bc_led_t led;
 bc_led_strip_t led_strip;
-
+uint32_t color;
+int cnt = 0;
 
 void application_init(void)
 {
@@ -20,7 +21,8 @@ void application_init(void)
 
 
     bc_module_power_init();
-    bc_module_power_led_strip_init(&led_strip, &bc_module_power_led_strip_buffer_rgbw_144);
+    bc_module_power_led_strip_init(&led_strip, &bc_module_power_led_strip_buffer_rgb_150);
+    bc_led_strip_set_event_handler(&led_strip, led_strip_event_handler, NULL);
 
     bc_led_set_mode(&led, BC_LED_MODE_OFF);
 }
@@ -32,7 +34,17 @@ void button_event_handler(bc_button_t *self, bc_button_event_t event, void *even
 
     if (event == BC_BUTTON_EVENT_PRESS)
     {
-        bc_led_strip_effect_rainbow_cycle(&led_strip);
+        if (cnt++ % 2)
+        {
+            color = 0xff000000;
+            bc_led_strip_fill(&led_strip, 0x00000000);
+            bc_led_strip_effect_color_wipe(&led_strip, color, 50);
+        }
+        else
+        {
+            color = 0x00000000;
+            bc_led_strip_effect_rainbow_cycle(&led_strip, 25);
+        }
     }
     else if (event == BC_BUTTON_EVENT_RELEASE)
     {
@@ -43,4 +55,18 @@ void button_event_handler(bc_button_t *self, bc_button_event_t event, void *even
         bc_led_strip_test(&led_strip);
     }
 
+}
+
+void led_strip_event_handler(bc_led_strip_t *self, bc_led_strip_event_t event, void *event_param)
+{
+    (void) event_param;
+
+    if (event == BC_LED_STRIP_EVENT_EFFECT_DONE)
+    {
+        color >>= 8;
+        if (color != 0x00000000)
+        {
+            bc_led_strip_effect_color_wipe(self, color, 50);
+        }
+    }
 }
