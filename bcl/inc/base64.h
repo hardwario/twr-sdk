66835d958fc156a04e5c0a5289f8e6d06026f137
Karel Blavka <karel.blavka@bigclown.com>
2017-03-14 15:27:46 +0100

    Base64 add calculate function

--- bcl/inc/base64.h
+++ bcl/inc/base64.h
@@ -5,5 +5,7 @@
 
 bool base64_encode(const uint8_t *input, uint32_t input_length, char *output, uint32_t *output_length);
 bool base64_decode(const char *input, uint32_t input_length, uint8_t *output, uint32_t *output_length);
+size_t base64_calculate_encode_length(size_t length);
+size_t base64_calculate_decode_length(const char *input, size_t length);
 
 #endif
