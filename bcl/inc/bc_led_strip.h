5f6eb344f5f9df6152e772543fa191d041f88c7b
Karel Blavka <karel.blavka@bigclown.com>
2017-03-14 15:27:20 +0100

    Ws2812b remove local copy count and type Fix dma size Remove const for led_strip_buffer

--- bcl/inc/bc_led_strip.h
+++ bcl/inc/bc_led_strip.h
@@ -20,7 +20,7 @@ typedef struct
 
 typedef struct
 {
-    bool (*init)(const bc_led_strip_buffer_t *buffer);
+    bool (*init)(bc_led_strip_buffer_t *buffer);
     void (*set_pixel)(int position, uint32_t color);
     void (*set_pixel_rgbw)(int position, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
     bool (*write)(void);
@@ -38,7 +38,7 @@ typedef struct bc_led_strip_t bc_led_strip_t;
 struct bc_led_strip_t
 {
     const bc_led_strip_driver_t *_driver;
-    const bc_led_strip_buffer_t *_buffer;
+    bc_led_strip_buffer_t *_buffer;
 
     struct
     {
@@ -54,7 +54,7 @@ struct bc_led_strip_t
 
 };
 
-void bc_led_strip_init(bc_led_strip_t *self, const bc_led_strip_driver_t *driver, const bc_led_strip_buffer_t *buffer);
+void bc_led_strip_init(bc_led_strip_t *self, const bc_led_strip_driver_t *driver, bc_led_strip_buffer_t *buffer);
 
 void bc_led_strip_set_event_handler(bc_led_strip_t *self, void (*event_handler)(bc_led_strip_t *, bc_led_strip_event_t, void *), void *event_param);
 
@@ -66,7 +66,7 @@ void bc_led_strip_set_pixel(bc_led_strip_t *self, int position, uint32_t color);
 
 void bc_led_strip_set_pixel_rgbw(bc_led_strip_t *self, int position, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 
-bool bc_led_strip_set_rgbw_framebuffer(bc_led_strip_t *self, const uint8_t *framebuffer, size_t length);
+bool bc_led_strip_set_rgbw_framebuffer(bc_led_strip_t *self, uint8_t *framebuffer, size_t length);
 
 void bc_led_strip_fill(bc_led_strip_t *self, uint32_t color);
 
