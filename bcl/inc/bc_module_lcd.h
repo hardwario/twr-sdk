c63ce883b20dcb46f8cda36b8a3e666e08109ab6
Karel Blavka <karel.blavka@bigclown.com>
2017-03-28 14:47:44 +0200

    Update doc for lcd module.

--- bcl/inc/bc_module_lcd.h
+++ bcl/inc/bc_module_lcd.h
@@ -43,20 +43,67 @@ typedef enum
 bc_module_lcd_framebuffer_t _bc_module_lcd_framebuffer;
 
 //! @brief Initialize lcd
+//! @param[in] bc_module_lcd_framebuffer_t framebuffer
 
 void bc_module_lcd_init(bc_module_lcd_framebuffer_t *framebuffer);
+
+//! @brief Lcd on
+
 void bc_module_lcd_on(void);
+
+//! @brief Lcd off
+
 void bc_module_lcd_off(void);
+
+//! @brief Lcd clear
+
 void bc_module_lcd_clear(void);
-void bc_module_lcd_draw_pixel(int x, int y, bool value);
+
+//! @brief Lcd draw pixel
+//! @param[in] int left
+//! @param[in] int top
+//! @param[in] bool value
+
+void bc_module_lcd_draw_pixel(int left, int top, bool value);
+
+//! @brief Lcd draw char
+//! @param[in] int left
+//! @param[in] int top
+//! @param[in] uint8_t ch
+//! @return width of printed character
+
 int bc_module_lcd_draw_char(int left, int top, uint8_t ch);
+
+//! @brief Lcd draw string
+//! @param[in] int left
+//! @param[in] int top
+//! @param[in] char *str
+//! @return width of printed characters
+
 int bc_module_lcd_draw_string(int left, int top, char *str);
+
 //void bc_module_lcd_draw(const uint8_t *frame, uint8_t width, uint8_t height); // In pixels
 //void bc_module_lcd_printf(uint8_t line, /*uint8_t size, font, */const uint8_t *string/*, ...*/);
+
+//! @brief Lcd update, send data
+
 void bc_module_lcd_update(void);
+
+//! @brief Lcd set font
+//! @param[in] tFont *font
+
 void bc_module_lcd_set_font(const tFont *font);
+
+//! @brief Lcd set rotation
+//! @param[in] bc_module_lcd_rotation_t rotation
+
 void bc_module_lcd_set_rotation(bc_module_lcd_rotation_t rotation);
+
+//! @brief Lcd get rotation
+//! @return bc_module_lcd_rotation_t
+
 bc_module_lcd_rotation_t bc_module_lcd_get_rotation(void);
+
 //! @}
 
 #endif /* _BC_MODULE_LCD */
