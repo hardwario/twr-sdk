da7c250740a57237248fd5f28a2e2d5158c1c8a3
Karel Blavka <karel.blavka@bigclown.com>
2017-04-05 13:56:56 +0200

    Update bc_usb_cdc task and add start function

--- bcl/inc/bc_usb_cdc.h
+++ bcl/inc/bc_usb_cdc.h
@@ -4,6 +4,7 @@
 #include <bc_common.h>
 
 void bc_usb_cdc_init(void);
+void bc_usb_cdc_start(void);
 bool bc_usb_cdc_write(const void *buffer, size_t length);
 size_t bc_usb_cdc_read(void *buffer, size_t length);
 
