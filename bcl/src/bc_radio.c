c4f04c222f860ce470007c31f1e1a82ad3bd541b
Karel Blavka <karel.blavka@bigclown.com>
2017-04-12 12:51:52 +0200

    Radio add pub buffer

--- bcl/src/bc_radio.c
+++ bcl/src/bc_radio.c
@@ -16,7 +16,8 @@ typedef enum
     BC_RADIO_HEADER_PUB_HUMIDITY,
     BC_RADIO_HEADER_PUB_LUX_METER,
     BC_RADIO_HEADER_PUB_BAROMETER,
-    BC_RADIO_HEADER_PUB_CO2
+    BC_RADIO_HEADER_PUB_CO2,
+    BC_RADIO_HEADER_PUB_BUFFER
 
 } bc_radio_header_t;
 
@@ -63,7 +64,7 @@ __attribute__((weak)) void bc_radio_on_humidity(uint32_t *peer_device_address, u
 __attribute__((weak)) void bc_radio_on_lux_meter(uint32_t *peer_device_address, uint8_t *i2c, float *illuminance) { (void) peer_device_address; (void) i2c; (void) illuminance; }
 __attribute__((weak)) void bc_radio_on_barometer(uint32_t *peer_device_address, uint8_t *i2c, float *pressure, float *altitude) { (void) peer_device_address; (void) i2c; (void) pressure; (void) altitude; }
 __attribute__((weak)) void bc_radio_on_co2(uint32_t *peer_device_address, int16_t *concentration) { (void) peer_device_address; (void) concentration; }
-__attribute__((weak)) void bc_radio_on_buffer(uint32_t *peer_device_address, void *buffer, size_t *length) { (void) peer_device_address; (void) buffer; (void) length; }
+__attribute__((weak)) void bc_radio_on_buffer(uint32_t *peer_device_address, uint8_t *buffer, size_t *length) { (void) peer_device_address; (void) buffer; (void) length; }
 
 
 void bc_radio_init(void)
@@ -233,6 +234,30 @@ bool bc_radio_pub_co2(int16_t *concentration)
     return true;
 }
 
+bool bc_radio_pub_buffer(uint8_t *buffer, size_t length)
+{
+    uint8_t qbuffer[1 + BC_SPIRIT1_MAX_PACKET_SIZE - 6];
+
+    if (length > (1 + BC_SPIRIT1_MAX_PACKET_SIZE - 6))
+    {
+        return false;
+    }
+
+    qbuffer[0] = BC_RADIO_HEADER_PUB_BUFFER;
+
+    memcpy(&qbuffer[1], buffer, length);
+
+    if (!bc_queue_put(&_bc_radio.pub_queue, qbuffer, length + 1))
+    {
+        return false;
+    }
+
+    bc_scheduler_plan_now(_bc_radio.task_id);
+
+    return true;
+
+}
+
 static void _bc_radio_task(void *param)
 {
     (void) param;
@@ -329,9 +354,10 @@ static void _bc_radio_task(void *param)
 
             bc_radio_on_co2(&_bc_radio.peer_device_address, &concentration);
         }
-        else
+        else if (queue_item_buffer[0] == BC_RADIO_HEADER_PUB_BUFFER)
         {
-            bc_radio_on_buffer(&_bc_radio.peer_device_address, queue_item_buffer, &queue_item_length);
+            queue_item_length -= 1;
+            bc_radio_on_buffer(&_bc_radio.peer_device_address, &queue_item_buffer[1], &queue_item_length);
         }
 
     }
