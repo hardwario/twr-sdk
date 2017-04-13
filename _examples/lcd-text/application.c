ece4c72b35af8fdbf4bc87a7dcb62106566f772b
hubmartin <hub.martin@gmail.com>
2017-04-13 13:25:06 +0200

    Fix classnames, font heights and font names

--- _examples/lcd-text/application.c
+++ _examples/lcd-text/application.c
@@ -5,19 +5,20 @@ void application_init(void)
     // Initialize LCD
     // The parameter is internal buffer in SDK, no need to define it
     bc_module_lcd_init(&_bc_module_lcd_framebuffer);
+
     // Init default font, this is necessary
     // See other fonts in sdk/bcl/inc/bc_font_common.h
-    bc_module_lcd_set_font(&bc_font_ubuntu12);
+    bc_module_lcd_set_font(&bc_font_ubuntu_15);
 
     // Draw string at X, Y location
     bc_module_lcd_draw_string(10, 5, "Hello world!");
 
     // Use big font
-    bc_module_lcd_set_font(&bc_font_ubuntu24);
+    bc_module_lcd_set_font(&bc_font_ubuntu_24);
     bc_module_lcd_draw_string(10, 40, "Big");
 
     // Set back default font
-    bc_module_lcd_set_font(&bc_font_ubuntu12);
+    bc_module_lcd_set_font(&bc_font_ubuntu_15);
     bc_module_lcd_draw_string(60, 50, "world");
 
     // Don't forget to update
--- fonts/README.md
+++ fonts/README.md
@@ -7,9 +7,9 @@ In  the Options you have to import Config_preset_monochrome_ISO8859-2.xml to set
 
 Add your new fonts to the sdk/bcl/inc/bc_font_common.h:
 
-extern const tFont YourNewFontName;
+extern const bc_font_t YourNewFontName;
 
-The format for new fonts is 
+The format for new fonts is
 bc_font_[name][size][_bold|_italic]
 
 The height of the font in the font name is the one used font size. The real height of the generated bitmap font could be higher.
