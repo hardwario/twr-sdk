35b6686529c8e4c71052833d1e97090dab424cc9
Petr Konvicka <petr.konvicka@bigclown.com>
2017-04-20 11:13:23 +0200

    hal unhaled

--- bcl/src/bc_i2c.c
+++ bcl/src/bc_i2c.c
@@ -1,5 +1,6 @@
 #include <bc_i2c.h>
 #include <bc_module_core.h>
+#include <bc_tick.h>
 #include <stm32l0xx.h>
 
 static struct
@@ -11,10 +12,20 @@ static struct
     I2C_HandleTypeDef handle_i2c1;
 
 } bc_i2c =
-{
-    .i2c0_initialized = false,
-    .i2c1_initialized = false
-};
+        {
+                .i2c0_initialized = false,
+                .i2c1_initialized = false
+        };
+
+static bool _I2C_Mem_Write(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+static bool _I2C_Mem_Read(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+static bool _I2C_RequestMemoryWrite(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint32_t Timeout, uint32_t Tickstart);
+static bool _I2C_RequestMemoryRead(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint32_t Timeout, uint32_t Tickstart);
+static void _config(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint8_t Size, uint32_t Mode, uint32_t Request);
+static bool _watch_flag_txis(I2C_TypeDef *I2Cx, uint32_t Timeout, uint32_t Tickstart);
+static bool _watch_flag(I2C_TypeDef *I2Cx, uint32_t Flag, FlagStatus Status, uint32_t Timeout, uint32_t Tickstart);
+static bool _watch_flag_stop(I2C_TypeDef *I2Cx, uint32_t Timeout, uint32_t Tickstart);
+static bool _ack_failed(I2C_TypeDef *I2Cx, uint32_t Timeout, uint32_t Tickstart);
 
 void bc_i2c_init(bc_i2c_channel_t channel, bc_i2c_speed_t speed)
 {
@@ -25,23 +36,20 @@ void bc_i2c_init(bc_i2c_channel_t channel, bc_i2c_speed_t speed)
             return;
         }
 
-        __HAL_RCC_GPIOB_CLK_ENABLE();
+        // Initialize I2C0 pins
+        RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
+        GPIOB->MODER &= ~(GPIO_MODER_MODE10_0 | GPIO_MODER_MODE11_0);
+        GPIOB->OTYPER |= GPIO_OTYPER_OT_10 | GPIO_OTYPER_OT_11;
+        GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEED10 | GPIO_OSPEEDER_OSPEED11; // TODO Mask by 0 first
+        GPIOB->PUPDR |= GPIO_PUPDR_PUPD10_0 | GPIO_PUPDR_PUPD11_0;
+        GPIOB->AFR[1] |= 0x6600;    // TODO ... mask by ffff00ff first
 
-        GPIO_InitTypeDef GPIO_InitStruct;
-
-        GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_11;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-        GPIO_InitStruct.Pull = GPIO_PULLUP;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = GPIO_AF6_I2C2;
-
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        __HAL_RCC_I2C2_CLK_ENABLE();
+        // Enable I2C0 peripheral
+        RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
 
         bc_i2c.handle_i2c0.Instance = I2C2;
 
-        if(speed == BC_I2C_SPEED_400_KHZ)
+        if (speed == BC_I2C_SPEED_400_KHZ)
         {
             bc_i2c.handle_i2c0.Init.Timing = 0x301110;
         }
@@ -52,29 +60,20 @@ void bc_i2c_init(bc_i2c_channel_t channel, bc_i2c_speed_t speed)
         else
         {
             // TODO Replace this
-            for (;;);
+            for (;;)
+                ;
         }
 
-        bc_i2c.handle_i2c0.Init.OwnAddress1 = 0;
-        bc_i2c.handle_i2c0.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-        bc_i2c.handle_i2c0.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-        bc_i2c.handle_i2c0.Init.OwnAddress2 = 0;
-        bc_i2c.handle_i2c0.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-        bc_i2c.handle_i2c0.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-        bc_i2c.handle_i2c0.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+        // Initialize I2C2 peripheral
+        I2C2->CR1 &= I2C_CR1_PE;
+        I2C2->CR2 = I2C_CR2_AUTOEND;
+        I2C2->OAR1 = I2C_OAR1_OA1EN;
+        I2C2->TIMINGR = bc_i2c.handle_i2c0.Init.Timing;
+        // Filter is set to analog, after reset
+        bc_i2c.handle_i2c0.State = HAL_I2C_STATE_READY;
+        I2C2->CR1 |= I2C_CR1_PE;
 
-        if (HAL_I2C_Init(&bc_i2c.handle_i2c0) != HAL_OK)
-        {
-            // TODO Replace this
-            for (;;);
-        }
-
-        if (HAL_I2CEx_ConfigAnalogFilter(&bc_i2c.handle_i2c0, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
-        {
-            // TODO Replace this
-            for (;;);
-        }
-
+        // Update
         bc_i2c.i2c0_initialized = true;
     }
     else
@@ -84,19 +83,15 @@ void bc_i2c_init(bc_i2c_channel_t channel, bc_i2c_speed_t speed)
             return;
         }
 
-        __HAL_RCC_GPIOB_CLK_ENABLE();
+        // Initialize I2C1 pins
+        RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
+        GPIOB->MODER &= ~(GPIO_MODER_MODE8_0 | GPIO_MODER_MODE9_0);
+        GPIOB->OTYPER |= GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9;
+        GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEED8 | GPIO_OSPEEDER_OSPEED9; // TODO Mask by 0 first
+        GPIOB->PUPDR |= GPIO_PUPDR_PUPD8_0 | GPIO_PUPDR_PUPD9_0;
+        GPIOB->AFR[1] |= 0x44;    // TODO ... mask by ffffff00 first
 
-        GPIO_InitTypeDef GPIO_InitStruct;
-
-        GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-        GPIO_InitStruct.Pull = GPIO_PULLUP;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
-
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        __HAL_RCC_I2C1_CLK_ENABLE();
+        RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
 
         bc_i2c.handle_i2c1.Instance = I2C1;
 
@@ -111,29 +106,20 @@ void bc_i2c_init(bc_i2c_channel_t channel, bc_i2c_speed_t speed)
         else
         {
             // TODO Replace this
-            for (;;);
+            for (;;)
+                ;
         }
 
-        bc_i2c.handle_i2c1.Init.OwnAddress1 = 0;
-        bc_i2c.handle_i2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-        bc_i2c.handle_i2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-        bc_i2c.handle_i2c1.Init.OwnAddress2 = 0;
-        bc_i2c.handle_i2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-        bc_i2c.handle_i2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-        bc_i2c.handle_i2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+        // Initialize I2C2 peripheral
+        I2C1->CR1 &= I2C_CR1_PE;
+        I2C1->CR2 = I2C_CR2_AUTOEND;
+        I2C1->OAR1 = I2C_OAR1_OA1EN;
+        I2C1->TIMINGR = bc_i2c.handle_i2c0.Init.Timing;
+        // Filter is set to analog, after reset
+        bc_i2c.handle_i2c1.State = HAL_I2C_STATE_READY;
+        I2C1->CR1 |= I2C_CR1_PE;
 
-        if (HAL_I2C_Init(&bc_i2c.handle_i2c1) != HAL_OK)
-        {
-            // TODO Replace this
-            for (;;);
-        }
-
-        if (HAL_I2CEx_ConfigAnalogFilter(&bc_i2c.handle_i2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
-        {
-            // TODO Replace this
-            for (;;);
-        }
-
+        // Update
         bc_i2c.i2c1_initialized = true;
     }
 }
@@ -150,7 +136,8 @@ bool bc_i2c_write(bc_i2c_channel_t channel, const bc_i2c_tranfer_t *transfer)
         // Enable PLL and disable sleep
         bc_module_core_pll_enable();
 
-        if (HAL_I2C_Mem_Write(&bc_i2c.handle_i2c0, transfer->device_address << 1, transfer->memory_address, (transfer->memory_address & BC_I2C_MEMORY_ADDRESS_16_BIT) != 0 ? I2C_MEMADD_SIZE_16BIT : I2C_MEMADD_SIZE_8BIT, transfer->buffer, transfer->length, 0xFFFFFFFF) != HAL_OK)
+        if (_I2C_Mem_Write(I2C2, transfer->device_address << 1, transfer->memory_address,
+                (transfer->memory_address & BC_I2C_MEMORY_ADDRESS_16_BIT) != 0 ? I2C_MEMADD_SIZE_16BIT : I2C_MEMADD_SIZE_8BIT, transfer->buffer, transfer->length, 10) != true)
         {
             // Disable PLL and enable sleep
             bc_module_core_pll_disable();
@@ -173,7 +160,8 @@ bool bc_i2c_write(bc_i2c_channel_t channel, const bc_i2c_tranfer_t *transfer)
         // Enable PLL and disable sleep
         bc_module_core_pll_enable();
 
-        if (HAL_I2C_Mem_Write(&bc_i2c.handle_i2c1, transfer->device_address << 1, transfer->memory_address, (transfer->memory_address & BC_I2C_MEMORY_ADDRESS_16_BIT) != 0 ? I2C_MEMADD_SIZE_16BIT : I2C_MEMADD_SIZE_8BIT, transfer->buffer, transfer->length, 0xFFFFFFFF) != HAL_OK)
+        if (_I2C_Mem_Write(I2C1, transfer->device_address << 1, transfer->memory_address,
+                (transfer->memory_address & BC_I2C_MEMORY_ADDRESS_16_BIT) != 0 ? I2C_MEMADD_SIZE_16BIT : I2C_MEMADD_SIZE_8BIT, transfer->buffer, transfer->length, 10) != true)
         {
             // Disable PLL and enable sleep
             bc_module_core_pll_disable();
@@ -200,7 +188,8 @@ bool bc_i2c_read(bc_i2c_channel_t channel, const bc_i2c_tranfer_t *transfer)
         // Enable PLL and disable sleep
         bc_module_core_pll_enable();
 
-        if (HAL_I2C_Mem_Read(&bc_i2c.handle_i2c0, transfer->device_address << 1, transfer->memory_address, (transfer->memory_address & BC_I2C_MEMORY_ADDRESS_16_BIT) != 0 ? I2C_MEMADD_SIZE_16BIT : I2C_MEMADD_SIZE_8BIT, transfer->buffer, transfer->length, 0xFFFFFFFF) != HAL_OK)
+        if (_I2C_Mem_Read(I2C2, transfer->device_address << 1, transfer->memory_address,
+                (transfer->memory_address & BC_I2C_MEMORY_ADDRESS_16_BIT) != 0 ? I2C_MEMADD_SIZE_16BIT : I2C_MEMADD_SIZE_8BIT, transfer->buffer, transfer->length, 10) != true)
         {
             // Disable PLL and enable sleep
             bc_module_core_pll_disable();
@@ -223,7 +212,8 @@ bool bc_i2c_read(bc_i2c_channel_t channel, const bc_i2c_tranfer_t *transfer)
         // Enable PLL and disable sleep
         bc_module_core_pll_enable();
 
-        if (HAL_I2C_Mem_Read(&bc_i2c.handle_i2c1, transfer->device_address << 1, transfer->memory_address, (transfer->memory_address & BC_I2C_MEMORY_ADDRESS_16_BIT) != 0 ? I2C_MEMADD_SIZE_16BIT : I2C_MEMADD_SIZE_8BIT, transfer->buffer, transfer->length, 0xFFFFFFFF) != HAL_OK)
+        if (_I2C_Mem_Read(I2C1, transfer->device_address << 1, transfer->memory_address,
+                (transfer->memory_address & BC_I2C_MEMORY_ADDRESS_16_BIT) != 0 ? I2C_MEMADD_SIZE_16BIT : I2C_MEMADD_SIZE_8BIT, transfer->buffer, transfer->length, 10) != true)
         {
             // Disable PLL and enable sleep
             bc_module_core_pll_disable();
@@ -297,5 +287,323 @@ bool bc_i2c_read_16b(bc_i2c_channel_t channel, uint8_t device_address, uint32_t 
 
     *data = buffer[0] << 8 | buffer[1];
 
+    return true;
+}
+
+
+
+
+
+
+
+static bool _I2C_Mem_Write(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+    uint32_t tickstart = 0U;
+
+    /* Init tickstart for timeout management*/
+    tickstart = bc_tick_get();
+
+    // Wait till I2Cx is busy
+    if (_watch_flag(I2Cx, I2C_ISR_BUSY, SET, 25, tickstart) != true)
+    {
+        return false;
+    }
+
+    // Send Slave Address and Memory Address
+    if (_I2C_RequestMemoryWrite(I2Cx, DevAddress, MemAddress, MemAddSize, Timeout, tickstart) != true)
+    {
+        return false;
+    }
+
+    // Set NBYTES to write
+    _config(I2Cx, DevAddress, Size, I2C_AUTOEND_MODE, I2C_NO_STARTSTOP);
+
+    do
+    {
+        /* Wait until TXIS flag is set */
+        if (_watch_flag_txis(I2Cx, Timeout, tickstart) != true)
+        {
+            return false;
+        }
+
+        /* Write data to TXDR */
+        I2Cx->TXDR = (*pData++);
+        Size--;
+
+    } while (Size > 0U);
+
+    /* No need to Check TC flag, with AUTOEND mode the stop is automatically generated */
+    /* Wait until STOPF flag is reset */
+    if (_watch_flag_stop(I2Cx, Timeout, tickstart) != true)
+    {
+        return false;
+    }
+
+    /* Clear STOP Flag */
+    I2Cx->ICR = I2C_ISR_STOPF;
+
+    /* Clear Configuration Register 2 */
+    I2Cx->CR2 &= (uint32_t)~((uint32_t)(I2C_CR2_SADD | I2C_CR2_HEAD10R | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_RD_WRN));
+
+    return true;
+}
+
+static bool _I2C_Mem_Read(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+  uint32_t tickstart = 0U;
+
+  // TODO vymazat, akorát zakryju chybu programátora
+    if((pData == NULL) || (Size == 0U))
+    {
+      return  false;
+    }
+
+    /* Init tickstart for timeout management*/
+    tickstart = bc_tick_get();
+
+    if(_watch_flag(I2Cx, I2C_ISR_BUSY, SET, 25, tickstart) != true)
+    {
+      return false;
+    }
+
+    /* Send Slave Address and Memory Address */
+    if(_I2C_RequestMemoryRead(I2Cx, DevAddress, MemAddress, MemAddSize, Timeout, tickstart) != true)
+    {
+        return false;
+    }
+
+    /* Send Slave Address */
+
+      _config(I2Cx, DevAddress, Size, I2C_CR2_AUTOEND, (uint32_t)(I2C_CR2_START | I2C_CR2_RD_WRN));
+
+    do
+    {
+      /* Wait until RXNE flag is set */
+      if(_watch_flag(I2Cx, I2C_ISR_RXNE, RESET, Timeout, tickstart) != true)
+      {
+        return false;
+      }
+
+      /* Read data from RXDR */
+      (*pData++) = I2Cx->RXDR;
+      Size--;
+
+    }while(Size > 0U);
+
+    /* No need to Check TC flag, with AUTOEND mode the stop is automatically generated */
+    /* Wait until STOPF flag is reset */
+    if(_watch_flag_stop(I2Cx, Timeout, tickstart) != true)
+    {
+        return false;
+    }
+
+    /* Clear STOP Flag */
+    I2Cx->ICR = I2C_ISR_STOPF;
+
+    /* Clear Configuration Register 2 */
+    I2Cx->CR2 &= (uint32_t)~((uint32_t)(I2C_CR2_SADD | I2C_CR2_HEAD10R | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_RD_WRN));
+
+    return true;
+}
+
+static bool _I2C_RequestMemoryWrite(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint32_t Timeout, uint32_t Tickstart)
+{
+    _config(I2Cx, DevAddress, MemAddSize, I2C_RELOAD_MODE, I2C_GENERATE_START_WRITE);
+
+    /* Wait until TXIS flag is set */
+    if (_watch_flag_txis(I2Cx, Timeout, Tickstart) != true)
+    {
+        return false;
+    }
+
+    /* If Memory address size is 8Bit */
+    if (MemAddSize == I2C_MEMADD_SIZE_8BIT)
+    {
+        /* Send Memory Address */
+        I2Cx->TXDR = MemAddress & 0xff;
+    }
+    else
+    {
+      /* Send MSB of Memory Address */
+      I2Cx->TXDR = (MemAddress >> 8) & 0xff;
+
+      /* Wait until TXIS flag is set */
+      if(_watch_flag_txis(I2Cx, Timeout, Tickstart) != true)
+      {
+          return false;
+      }
+
+      /* Send LSB of Memory Address */
+      I2Cx->TXDR = MemAddress & 0xff;
+    }
+
+    /* Wait until TCR flag is set */
+    if (_watch_flag(I2Cx, I2C_FLAG_TCR, RESET, Timeout, Tickstart) != true)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+static bool _I2C_RequestMemoryRead(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint32_t Timeout, uint32_t Tickstart)
+{
+  _config(I2Cx,DevAddress,MemAddSize, I2C_SOFTEND_MODE, I2C_GENERATE_START_WRITE);
+
+  /* Wait until TXIS flag is set */
+  if(_watch_flag_txis(I2Cx, Timeout, Tickstart) != true)
+  {
+      return false;
+  }
+
+  /* If Memory address size is 8Bit */
+  if(MemAddSize == I2C_MEMADD_SIZE_8BIT)
+  {
+    /* Send Memory Address */
+      I2Cx->TXDR = MemAddress & 0xff;
+  }
+  /* If Memory address size is 16Bit */
+  else
+  {
+    /* Send MSB of Memory Address */
+      I2Cx->TXDR = (MemAddress << 8) & 0xff;
+
+    /* Wait until TXIS flag is set */
+    if(_watch_flag_txis(I2Cx, Timeout, Tickstart) != true)
+    {
+        return false;
+    }
+
+    /* Send LSB of Memory Address */
+    I2Cx->TXDR = MemAddress & 0xff;
+  }
+
+  /* Wait until TC flag is set */
+  if(_watch_flag(I2Cx, I2C_ISR_TC, RESET, Timeout, Tickstart) != true)
+  {
+    return false;
+  }
+
+  return true;
+}
+
+static void _config(I2C_TypeDef *I2Cx, uint16_t DevAddress, uint8_t Size, uint32_t Mode, uint32_t Request)
+{
+    uint32_t tmpreg = 0U;
+
+    /* Get the CR2 register value */
+    tmpreg = I2Cx->CR2;
+
+    /* clear tmpreg specific bits */
+    tmpreg &= (uint32_t) ~((uint32_t) (I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_AUTOEND | I2C_CR2_RD_WRN | I2C_CR2_START | I2C_CR2_STOP));
+
+    /* update tmpreg */
+    tmpreg |= (uint32_t) (((uint32_t) DevAddress & I2C_CR2_SADD) | (((uint32_t) Size << 16) & I2C_CR2_NBYTES) |
+            (uint32_t) Mode | (uint32_t) Request);
+
+    /* update CR2 register */
+    I2Cx->CR2 = tmpreg;
+}
+
+static bool _watch_flag_txis(I2C_TypeDef *I2Cx, uint32_t Timeout, uint32_t Tickstart)
+{
+    while ((I2Cx->ISR & I2C_ISR_TXIS) == 0)
+    {
+        /* Check if a NACK is detected */
+        if (_ack_failed(I2Cx, Timeout, Tickstart) != true)
+        {
+            return false;
+        }
+
+        /* Check for the Timeout */
+        if (Timeout != HAL_MAX_DELAY)
+        {
+            if ((Timeout == 0U) || ((bc_tick_get() - Tickstart) > Timeout))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+static bool _watch_flag(I2C_TypeDef *I2Cx, uint32_t Flag, FlagStatus Status, uint32_t Timeout, uint32_t Tickstart)
+{
+    while ((I2Cx->ISR & Flag) == Status)
+    {
+        /* Check for the Timeout */
+        if (Timeout != HAL_MAX_DELAY)
+        {
+            if ((Timeout == 0U) || ((bc_tick_get() - Tickstart) > Timeout))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+static bool _watch_flag_stop(I2C_TypeDef *I2Cx, uint32_t Timeout, uint32_t Tickstart)
+{
+   while((I2Cx->ISR & I2C_ISR_STOPF) == 0)
+  {
+    /* Check if a NACK is detected */
+    if(_ack_failed(I2Cx, Timeout, Tickstart) != true)
+    {
+      return false;
+    }
+
+    /* Check for the Timeout */
+    if((Timeout == 0U)||((bc_tick_get() - Tickstart) > Timeout))
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+static bool _ack_failed(I2C_TypeDef *I2Cx, uint32_t Timeout, uint32_t Tickstart)
+{
+    if ((I2Cx->ISR & I2C_ISR_NACKF) == true)
+    {
+        /* Wait until STOP Flag is reset */
+        /* AutoEnd should be initiate after AF */
+        while ((I2Cx->ISR & I2C_ISR_STOPF) == 0)
+        {
+            /* Check for the Timeout */
+            if (Timeout != HAL_MAX_DELAY)
+            {
+                if ((Timeout == 0U) || ((bc_tick_get() - Tickstart) > Timeout))
+                {
+                    return false;
+                }
+            }
+        }
+
+        /* Clear NACKF Flag */
+        I2Cx->ICR = I2C_ISR_NACKF;
+
+        /* Clear STOP Flag */
+        I2Cx->ICR = I2C_ISR_STOPF;
+
+        /* Flush TX register */
+        /* If a pending TXIS flag is set */
+        /* Write a dummy data in TXDR to clear it */
+        if ((I2Cx->ISR & I2C_ISR_TXIS) != 0)
+        {
+            I2Cx->TXDR = 0x00U;
+        }
+
+        /* Flush TX register if not empty */
+        if ((I2Cx->ISR & I2C_ISR_TXE) == 0)
+        {
+            I2Cx->ISR |= I2C_ISR_TXE;
+        }
+
+        /* Clear Configuration Register 2 */
+        I2Cx->CR2 &= (uint32_t) ~((uint32_t) (I2C_CR2_SADD | I2C_CR2_HEAD10R | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_RD_WRN));
+
+        return false;
+    }
     return true;
 }
