6c8f37db636cd79792ef6d2e3cfd374d1ae83142
Petr Konvicka <petr.konvicka@bigclown.com>
2017-05-10 14:13:20 +0200

    Update documentation

--- bcl/inc/bc_gpio.h
+++ bcl/inc/bc_gpio.h
@@ -11,26 +11,65 @@
 
 typedef enum
 {
-    BC_GPIO_P0 = 0, //!< GPIO channel P0
-    BC_GPIO_P1 = 1, //!< GPIO channel P1
-    BC_GPIO_P2 = 2, //!< GPIO channel P2
-    BC_GPIO_P3 = 3, //!< GPIO channel P3
-    BC_GPIO_P4 = 4, //!< GPIO channel P4
-    BC_GPIO_P5 = 5, //!< GPIO channel P5
-    BC_GPIO_P6 = 6, //!< GPIO channel P6
-    BC_GPIO_P7 = 7, //!< GPIO channel P7
-    BC_GPIO_P8 = 8, //!< GPIO channel P8
-    BC_GPIO_P9 = 9, //!< GPIO channel P9
-    BC_GPIO_P10 = 10, //!< GPIO channel P10
-    BC_GPIO_P11 = 11, //!< GPIO channel P11
-    BC_GPIO_P12 = 12, //!< GPIO channel P12
-    BC_GPIO_P13 = 13, //!< GPIO channel P13
-    BC_GPIO_P14 = 14, //!< GPIO channel P14
-    BC_GPIO_P15 = 15, //!< GPIO channel P15
-    BC_GPIO_P16 = 16, //!< GPIO channel P16
-    BC_GPIO_P17 = 17, //!< GPIO channel P17
-    BC_GPIO_LED = 18, //!< GPIO channel LED
-    BC_GPIO_BUTTON = 19, //!< GPIO channel BUTTON
+    //!< GPIO channel P0
+    BC_GPIO_P0 = 0,
+
+    //!< GPIO channel P1
+    BC_GPIO_P1 = 1,
+
+    //!< GPIO channel P2
+    BC_GPIO_P2 = 2,
+
+    //!< GPIO channel P3
+    BC_GPIO_P3 = 3,
+
+    //!< GPIO channel P4
+    BC_GPIO_P4 = 4,
+
+    //!< GPIO channel P5
+    BC_GPIO_P5 = 5,
+
+    //!< GPIO channel P6
+    BC_GPIO_P6 = 6,
+
+    //!< GPIO channel P7
+    BC_GPIO_P7 = 7,
+
+    //!< GPIO channel P8
+    BC_GPIO_P8 = 8,
+
+    //!< GPIO channel P9
+    BC_GPIO_P9 = 9,
+
+    //!< GPIO channel P10
+    BC_GPIO_P10 = 10,
+
+    //!< GPIO channel P11
+    BC_GPIO_P11 = 11,
+
+    //!< GPIO channel P12
+    BC_GPIO_P12 = 12,
+
+    //!< GPIO channel P13
+    BC_GPIO_P13 = 13,
+
+    //!< GPIO channel P14
+    BC_GPIO_P14 = 14,
+
+    //!< GPIO channel P15
+    BC_GPIO_P15 = 15,
+
+    //!< GPIO channel P16
+    BC_GPIO_P16 = 16,
+
+    //!< GPIO channel P17
+    BC_GPIO_P17 = 17,
+
+    //!< GPIO channel LED
+    BC_GPIO_LED = 18,
+
+    //!< GPIO channel BUTTON
+    BC_GPIO_BUTTON = 19,
 
 } bc_gpio_channel_t;
 
@@ -38,9 +77,14 @@ typedef enum
 
 typedef enum
 {
-    BC_GPIO_PULL_NONE = 0, //!< GPIO channel has no pull-up/pull-down
-    BC_GPIO_PULL_UP = 1, //!< GPIO channel has pull-up
-    BC_GPIO_PULL_DOWN = 2 //!< GPIO channel has pull-down
+    //!< GPIO channel has no pull-up/pull-down
+    BC_GPIO_PULL_NONE = 0,
+
+    //!< GPIO channel has pull-up
+    BC_GPIO_PULL_UP = 1,
+
+    //!< GPIO channel has pull-down
+    BC_GPIO_PULL_DOWN = 2
 
 } bc_gpio_pull_t;
 
@@ -48,11 +92,20 @@ typedef enum
 
 typedef enum
 {
-    BC_GPIO_MODE_INPUT = 0, //!< GPIO channel operates as input
-    BC_GPIO_MODE_OUTPUT = 1, //!< GPIO channel operates as output
-    BC_GPIO_MODE_ALTERNATE = 2, //!< GPIO channel operates in alternate mode
-    BC_GPIO_MODE_ANALOG = 3, //!< GPIO channel operates in analog mode
-    BC_GPIO_MODE_OUTPUT_OD = 4 //!< GPIO channel operates as open-drain output
+    //!< GPIO channel operates as input
+    BC_GPIO_MODE_INPUT = 0,
+
+    //!< GPIO channel operates as output
+    BC_GPIO_MODE_OUTPUT = 1,
+
+    //!< GPIO channel operates in alternate mode
+    BC_GPIO_MODE_ALTERNATE = 2,
+
+    //!< GPIO channel operates in analog mode
+    BC_GPIO_MODE_ANALOG = 3,
+
+    //!< GPIO channel operates as open-drain output
+    BC_GPIO_MODE_OUTPUT_OD = 4
 
 } bc_gpio_mode_t;
 
