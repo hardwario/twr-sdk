06d6f0c19049c8f1018c84d486e42584e2e4f73c
Vladislav Vavra <vlvavra@cisco.com>
2017-06-02 16:52:32 +0200

    fix formatting - replace tabs by spaces

--- bcl/src/bc_module_sensor.c
+++ bcl/src/bc_module_sensor.c
@@ -43,18 +43,18 @@ bool bc_module_sensor_set_button_input_mode(bc_module_channel_t channel, void (*
 {
     switch(channel)
     {
-	case BC_MODULE_SENSOR_CHANNEL_A:
-	    bc_module_sensor_set_pull(BC_MODULE_SENSOR_CHANNEL_A, BC_MODULE_PULL_4K7);
-	    bc_button_init(&_bc_module_sensor.button_port_a, BC_GPIO_P4, BC_GPIO_PULL_UP, true);
+        case BC_MODULE_SENSOR_CHANNEL_A:
+            bc_module_sensor_set_pull(BC_MODULE_SENSOR_CHANNEL_A, BC_MODULE_PULL_4K7);
+            bc_button_init(&_bc_module_sensor.button_port_a, BC_GPIO_P4, BC_GPIO_PULL_UP, true);
             bc_button_set_event_handler(&_bc_module_sensor.button_port_a, event_handler, NULL);
-	    return true;
-	case BC_MODULE_SENSOR_CHANNEL_B:
-	    bc_module_sensor_set_pull(BC_MODULE_SENSOR_CHANNEL_B, BC_MODULE_PULL_4K7);
-	    bc_button_init(&_bc_module_sensor.button_port_b, BC_GPIO_P5, BC_GPIO_PULL_UP, true);
+            return true;
+        case BC_MODULE_SENSOR_CHANNEL_B:
+            bc_module_sensor_set_pull(BC_MODULE_SENSOR_CHANNEL_B, BC_MODULE_PULL_4K7);
+            bc_button_init(&_bc_module_sensor.button_port_b, BC_GPIO_P5, BC_GPIO_PULL_UP, true);
             bc_button_set_event_handler(&_bc_module_sensor.button_port_b, event_handler, NULL);
-	    return true;
-	default:
-	    return false;
+            return true;
+        default:
+            return false;
     }
 }
 
@@ -62,16 +62,16 @@ bool bc_module_sensor_set_digital_output_mode(bc_module_channel_t channel)
 {
     switch(channel)
     {
-	case BC_MODULE_SENSOR_CHANNEL_A:
-	    bc_gpio_init(BC_GPIO_P4);
+        case BC_MODULE_SENSOR_CHANNEL_A:
+            bc_gpio_init(BC_GPIO_P4);
             bc_gpio_set_mode(BC_GPIO_P4, BC_GPIO_MODE_OUTPUT);
-	    return true;
-	case BC_MODULE_SENSOR_CHANNEL_B:
-	    bc_gpio_init(BC_GPIO_P5);
+            return true;
+        case BC_MODULE_SENSOR_CHANNEL_B:
+            bc_gpio_init(BC_GPIO_P5);
             bc_gpio_set_mode(BC_GPIO_P5, BC_GPIO_MODE_OUTPUT);
-	    return true;
-	default:
-	    return false;
+            return true;
+        default:
+            return false;
     }
 }
 
@@ -112,12 +112,12 @@ void bc_module_sensor_set_digital_output(bc_module_channel_t channel, bool value
     bc_gpio_channel_t output_channel;
     switch(channel)
     {
-	case BC_MODULE_SENSOR_CHANNEL_A:
-	    output_channel = BC_GPIO_P4;
-	case BC_MODULE_SENSOR_CHANNEL_B:
-	    output_channel = BC_GPIO_P5;
-	default:
-	    output_channel = BC_GPIO_P4;
+        case BC_MODULE_SENSOR_CHANNEL_A:
+            output_channel = BC_GPIO_P4;
+        case BC_MODULE_SENSOR_CHANNEL_B:
+            output_channel = BC_GPIO_P5;
+        default:
+            output_channel = BC_GPIO_P4;
     }
     bc_gpio_set_output(output_channel, value ? 1 : 0);
 }
