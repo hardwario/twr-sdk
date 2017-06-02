23378a841bf6415088e3996c53000572d49a32da
Vladislav Vavra <vlvavra@cisco.com>
2017-06-02 16:43:40 +0200

    Add ability to connect simple button to sensor module

--- bcl/inc/bc_module_sensor.h
+++ bcl/inc/bc_module_sensor.h
@@ -2,6 +2,7 @@
 #define _BC_MODULE_SENSOR_H
 
 #include <bc_gpio.h>
+#include <bc_button.h>
 
 //! @addtogroup bc_module_sensor bc_module_sensor
 //! @brief Driver for Sensor Module
@@ -51,6 +52,14 @@ bool bc_module_sensor_set_pull(bc_module_channel_t channel, bc_module_pull_t pul
 //
 
 bool bc_module_sensor_set_digital_output_mode(bc_module_channel_t channel);
+
+//! @brief Set output mode of Sensor module to button behaviour
+//! @param[in] channel Sensor module channel
+//! @return true On success
+//! @return false On Error
+//
+
+bool bc_module_sensor_set_button_input_mode(bc_module_channel_t channel, void (*event_handler)(bc_button_t *, bc_button_event_t, void *));
 
 //! @brief Set digital output of Sensor module channel to given value
 //! @param[in] channel Sensor module channel
