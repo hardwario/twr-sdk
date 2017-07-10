e7061eb520b6e12e594b5b3f6f6dfa22f2d17c9c
konnev <petr.konvicka@bigclown.com>
2017-07-10 11:27:42 +0200

    Fix channel enum

--- bcl/inc/bc_module_sensor.h
+++ bcl/inc/bc_module_sensor.h
@@ -17,7 +17,7 @@ typedef enum
     //! @brief Channel B
     BC_MODULE_SENSOR_CHANNEL_B = 1
 
-} bc_module_channel_t;
+} bc_module_sensor_channel_t;
 
 //! @brief Sensor module pull
 
@@ -46,20 +46,20 @@ bool bc_module_sensor_init(void);
 //! @return true On success
 //! @return false On Error
 
-bool bc_module_sensor_set_pull(bc_module_channel_t channel, bc_module_sensor_pull_t pull);
+bool bc_module_sensor_set_pull(bc_module_sensor_channel_t channel, bc_module_sensor_pull_t pull);
 
 //! @brief Set digital output mode of Sensor module
 //! @param[in] channel Sensor module channel
 //! @return true On success
 //! @return false On Error
 
-bool bc_module_sensor_set_digital_output_mode(bc_module_channel_t channel);
+bool bc_module_sensor_set_digital_output_mode(bc_module_sensor_channel_t channel);
 
 //! @brief Set digital output of Sensor module channel to given value
 //! @param[in] channel Sensor module channel
 //! @param[in] value Value to be set
 
-void bc_module_sensor_set_digital_output(bc_module_channel_t channel, bool value);
+void bc_module_sensor_set_digital_output(bc_module_sensor_channel_t channel, bool value);
 
 //! @}
 
