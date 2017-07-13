3e416a6f37fd3bc438e5ba0c0627c69c46c86cff
Karel Blavka <karel.blavka@bigclown.com>
2017-07-13 12:14:44 +0200

    Update radio example

--- _examples/radio-base/application.h
+++ _examples/radio-base/application.h
@@ -3,10 +3,4 @@
 
 #include <bcl.h>
 
-// Forward declarations
-
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
-
-void radio_event_handler(bc_radio_event_t event, void *event_param);
-
 #endif // _APPLICATION_H
--- _examples/radio-remote/application.h
+++ _examples/radio-remote/application.h
@@ -3,12 +3,4 @@
 
 #include <bcl.h>
 
-// Forward declarations
-
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
-
-void temperature_tag_event_handler(bc_tag_temperature_t *self, bc_tag_temperature_event_t event, void *event_param);
-
-void radio_event_handler(bc_radio_event_t event, void *event_param);
-
 #endif // _APPLICATION_H
