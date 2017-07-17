a29bf15d63a8ea0969746cd85bd1ec2ac74de622
Karel Blavka <karel.blavka@bigclown.com>
2017-07-17 22:36:56 +0200

    Add 1wire relay

---
+++ bcl/inc/bc_1wire_relay.h
@@ -0,0 +1,64 @@
+#ifndef _BC_1WIRE_RELAY_H
+#define _BC_1WIRE_RELAY_H
+
+#include <bc_1wire.h>
+#include <bc_module_sensor.h>
+
+#define BC_1WIRE_RELAY_FAMILY_CODE 0x29
+
+//! @addtogroup bc_onewire_relay bc_onewire_relay
+//! @brief Driver for BigClown 1-wire relay, chipset: DS2408
+//! @{
+
+
+typedef enum
+{
+	BC_1WIRE_RELAY_Q1 = 0,
+	BC_1WIRE_RELAY_Q2 = 1,
+	BC_1WIRE_RELAY_Q3 = 2,
+	BC_1WIRE_RELAY_Q4 = 3,
+	BC_1WIRE_RELAY_Q5 = 4,
+	BC_1WIRE_RELAY_Q6 = 5,
+	BC_1WIRE_RELAY_Q7 = 6,
+	BC_1WIRE_RELAY_Q8 = 7,
+
+} bc_1wire_relay_q_t;
+
+//! @brief BigClown 1-wire relay instance
+
+typedef struct
+{
+	uint64_t _device_number;
+	bc_gpio_channel_t _channel;
+	uint8_t _state;
+
+} bc_1wire_relay_t;
+
+//! @brief Initialize relay
+//! @param[in] self Instance
+//! @return true On success
+//! @return false On failure
+
+bool bc_1wire_relay_init(bc_1wire_relay_t *self, uint64_t device_number, bc_module_sensor_channel_t channel);
+
+//! @brief Set relay to specified state
+//! @param[in] self Instance
+//! @param[in] q
+//! @param[in] state Desired relay state
+//! @return true On success
+//! @return false On failure
+
+bool bc_1wire_relay_set_state(bc_1wire_relay_t *self, bc_1wire_relay_q_t q, bool state);
+
+//! @brief Get current relay state
+//! @param[in] self Instance
+//! @param[in] q
+//! @param[out] state
+//! @return true When value is valid
+//! @return false When value is invalid
+
+bool bc_1wire_relay_get_state(bc_1wire_relay_t *self, bc_1wire_relay_q_t q, bool *state);
+
+//! @}
+
+#endif // _BC_1WIRE_RELAY_H
---
+++ bcl/src/bc_1wire_relay.c
@@ -0,0 +1,98 @@
+#include <bc_1wire_relay.h>
+#include <bc_gpio.h>
+
+bool _bc_onewire_relay_read_state(bc_1wire_relay_t *self);
+
+bool bc_1wire_relay_init(bc_1wire_relay_t *self, uint64_t device_number, bc_module_sensor_channel_t channel)
+{
+	memset(self, 0, sizeof(*self));
+	self->_device_number = device_number;
+
+	self->_channel = channel == BC_MODULE_SENSOR_CHANNEL_A ? BC_GPIO_P4 : BC_GPIO_P5;
+
+	bc_1wire_init(self->_channel);
+
+	if (!bc_module_sensor_init())
+	{
+		return false;
+	}
+
+	if (!bc_module_sensor_set_pull(channel, BC_MODULE_SENSOR_PULL_UP_4K7))
+	{
+		return false;
+	}
+
+	if (!_bc_onewire_relay_read_state(self))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool bc_1wire_relay_set_state(bc_1wire_relay_t *self, bc_1wire_relay_q_t q, bool state)
+{
+
+	if (!bc_1wire_reset(self->_channel))
+	{
+		return false;
+	}
+
+	uint8_t new_state = self->_state;
+
+	if (!state)
+	{
+		new_state |= (1 << q);
+	}
+	else
+	{
+		new_state &= ~(1 << q);
+	}
+
+	bc_1wire_select(self->_channel, &self->_device_number);
+
+	uint8_t buffer[] = {0x5A, new_state, ~new_state};
+
+	bc_1wire_write(self->_channel, buffer, sizeof(buffer));
+
+	if (bc_1wire_read_8b(self->_channel) != 0xAA)
+	{
+		return false;
+	}
+
+	if (bc_1wire_read_8b(self->_channel) != new_state)
+	{
+		return false;
+	}
+
+	self->_state = new_state;
+
+	return true;
+}
+
+
+bool bc_1wire_relay_get_state(bc_1wire_relay_t *self, bc_1wire_relay_q_t q, bool *state)
+{
+	*state = ((self->_state >> q) & 0x01) == 0x00;
+
+	return true;
+}
+
+bool _bc_onewire_relay_read_state(bc_1wire_relay_t *self)
+{
+	if (!bc_1wire_reset(self->_channel))
+	{
+		return false;
+	}
+
+	bc_1wire_select(self->_channel, &self->_device_number);
+
+	bc_1wire_write_8b(self->_channel, 0xF5);
+
+	self->_state = bc_1wire_read_8b(self->_channel);
+
+	return true;
+}
+
+
+
