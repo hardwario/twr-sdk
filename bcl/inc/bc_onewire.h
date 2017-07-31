d4a870ee216863747d068201cb36c562b627b8b7
Karel Blavka <karel.blavka@bigclown.com>
2017-07-31 08:21:02 +0200

    Add onewire

---
+++ bcl/inc/bc_onewire.h
@@ -0,0 +1,108 @@
+#ifndef _BC_1WIRE_H
+#define _BC_ONEWIRE_H
+
+#include <bc_gpio.h>
+
+//! @addtogroup bc_onewire bc_onewire
+//! @brief Driver for 1-Wire
+//! @{
+
+#define BC_ONEWIRE_DEVICE_NUMBER_SKIP_ROM 0
+
+//! @brief Initialize 1-Wire
+//! @param channel GPIO channel
+
+void bc_onewire_init(bc_gpio_channel_t channel);
+
+//! @brief Reset the 1-Wire bus and return the presence of any device
+//! @param channel GPIO channel
+//! @return true Device present
+//! @return false No device present
+
+bool bc_onewire_reset(bc_gpio_channel_t channel);
+
+//! @brief Select device
+//! @param channel GPIO channel
+//! @param[in] device_number Device number (for 0 skip ROM)
+
+void bc_onewire_select(bc_gpio_channel_t channel, uint64_t *device_number);
+
+//! @brief Skip ROM
+//! @param channel GPIO channel
+
+void bc_onewire_skip_rom(bc_gpio_channel_t channel);
+
+//! @brief Select device
+//! @param channel GPIO channel
+//! @param[in] data Input data to be written
+//! @param[in] length Number of bytes to be written
+
+void bc_onewire_write(bc_gpio_channel_t channel, const void *buffer, size_t length);
+
+//! @brief Select device
+//! @param channel GPIO channel
+//! @param[out] data Output which have been read
+//! @param[in] length Number of bytes to be read
+
+void bc_onewire_read(bc_gpio_channel_t channel, void *buffer, size_t length);
+
+//! @brief Select device
+//! @param channel GPIO channel
+//! @param[in] data Input data to be written
+
+void bc_onewire_write_8b(bc_gpio_channel_t channel, uint8_t data);
+
+//! @brief Select device
+//! @param channel GPIO channel
+//! @return data which have been read
+
+uint8_t bc_onewire_read_8b(bc_gpio_channel_t channel);
+
+//! @brief Select device
+//! @param channel GPIO channel
+//! @param[in] bit Input bit to be written
+
+void bc_onewire_write_bit(bc_gpio_channel_t channel, int bit);
+
+//! @brief Select device
+//! @param channel GPIO channel
+//! @return bit which have been read
+
+int bc_onewire_read_bit(bc_gpio_channel_t channel);
+
+//! @brief Search for all devices on 1-Wire
+//! @param[in] channel GPIO channel
+//! @param[out] device_list Pointer to destination array holding list of devices
+//! @param[in] device_list_size Size of array holding list of devices
+//! @return Number of found devices
+
+int bc_onewire_search_all(bc_gpio_channel_t channel, uint64_t *device_list, size_t device_list_size);
+
+//! @brief Search for all devices on 1-Wire with family code
+//! @param[in] channel GPIO channel
+//! @param[in] family_code
+//! @param[out] device_list Pointer to destination array holding list of devices
+//! @param[in] device_list_size Size of array holding list of devices
+//! @return Number of found devices
+
+int bc_onewire_search_family(bc_gpio_channel_t channel, uint8_t family_code, uint64_t *device_list, size_t device_list_size);
+
+//! @brief Calculate 8-bit CRC
+//! @param[in] buffer
+//! @param[in] length Number of bytes
+//! @param[in] The crc starting value
+//! @return Calculated CRC
+
+uint8_t bc_onewire_crc8(const void *buffer, size_t length, uint8_t crc);
+
+//! @brief Calculate 16-bit CRC
+//! @param[in] buffer
+//! @param[in] length Number of bytes
+//! @param[in] The crc starting value
+//! @return Calculated CRC
+
+uint16_t bc_onewire_crc16(const void *buffer, size_t length, uint16_t crc);
+
+//! @}
+
+#endif /* _BC_1WIRE_H */
