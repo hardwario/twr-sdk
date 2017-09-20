887fd6b148af88eb350c8abac8e4bf408b18c8df
Pavel Hübner <pavel.hubner@bigclown.com>
2017-09-20 12:19:44 +0200

    bc_log: Add logging library

---
+++ bcl/inc/bc_log.h
@@ -0,0 +1,59 @@
+#ifndef _BC_LOG_H
+#define _BC_LOG_H
+
+#include <bc_common.h>
+
+//! @addtogroup bc_log bc_log
+//! @brief Logging library (output at TXD2, parameters 115200 / 8N1)
+//! @{
+
+//! @brief Log level
+
+typedef enum
+{
+    //! @brief Log level DEBUG
+    BC_LOG_LEVEL_DEBUG = 0,
+
+    //! @brief Log level INFO
+    BC_LOG_LEVEL_INFO = 1,
+
+    //! @brief Log level WARNING
+    BC_LOG_LEVEL_WARNING = 2,
+
+    //! @brief Log level ERROR
+    BC_LOG_LEVEL_ERROR = 3
+
+} bc_log_level_t;
+
+//! @brief Initialize logging library
+//! @param[in] level Minimum required message level for propagation
+
+void bc_log_init(bc_log_level_t level);
+
+//! @brief Log DEBUG message (annotated in log as <D>)
+//! @param[in] format Format string (printf style)
+//! @param[in] ... Optional format arguments
+
+void bc_log_debug(const char *format, ...);
+
+//! @brief Log INFO message (annotated in log as <I>)
+//! @param[in] format Format string (printf style)
+//! @param[in] ... Optional format arguments
+
+void bc_log_info(const char *format, ...);
+
+//! @brief Log WARNING message (annotated in log as <W>)
+//! @param[in] format Format string (printf style)
+//! @param[in] ... Optional format arguments
+
+void bc_log_warning(const char *format, ...);
+
+//! @brief Log ERROR message (annotated in log as <E>)
+//! @param[in] format Format string (printf style)
+//! @param[in] ... Optional format arguments
+
+void bc_log_error(const char *format, ...);
+
+//! @}
+
+#endif // _BC_LOG_H
---
+++ bcl/src/bc_log.c
@@ -0,0 +1,99 @@
+#include <bc_log.h>
+#include <bc_uart.h>
+
+static struct
+{
+    bc_log_level_t level;
+
+    char buffer[256];
+
+} _bc_log;
+
+void bc_log_init(bc_log_level_t level)
+{
+    memset(&_bc_log, 0, sizeof(_bc_log));
+
+    _bc_log.level = level;
+
+    bc_uart_init(BC_UART_UART2, BC_UART_BAUDRATE_115200, BC_UART_SETTING_8N1);
+}
+
+void bc_log_debug(const char *format, ...)
+{
+    va_list ap;
+
+    if (_bc_log.level > BC_LOG_LEVEL_DEBUG)
+    {
+        return;
+    }
+
+    strcpy(_bc_log.buffer, "# <D> ");
+
+    va_start(ap, format);
+    vsnprintf(&_bc_log.buffer[6], sizeof(_bc_log.buffer) - 6 - 2, format, ap);
+    va_end(ap);
+
+    strcat(_bc_log.buffer, "\r\n");
+
+    bc_uart_write(BC_UART_UART2, _bc_log.buffer, strlen(_bc_log.buffer));
+}
+
+void bc_log_info(const char *format, ...)
+{
+    va_list ap;
+
+    if (_bc_log.level > BC_LOG_LEVEL_INFO)
+    {
+        return;
+    }
+
+    strcpy(_bc_log.buffer, "# <I> ");
+
+    va_start(ap, format);
+    vsnprintf(&_bc_log.buffer[6], sizeof(_bc_log.buffer) - 6 - 2, format, ap);
+    va_end(ap);
+
+    strcat(_bc_log.buffer, "\r\n");
+
+    bc_uart_write(BC_UART_UART2, _bc_log.buffer, strlen(_bc_log.buffer));
+}
+
+void bc_log_warning(const char *format, ...)
+{
+    va_list ap;
+
+    if (_bc_log.level > BC_LOG_LEVEL_WARNING)
+    {
+        return;
+    }
+
+    strcpy(_bc_log.buffer, "# <W> ");
+
+    va_start(ap, format);
+    vsnprintf(&_bc_log.buffer[6], sizeof(_bc_log.buffer) - 6 - 2, format, ap);
+    va_end(ap);
+
+    strcat(_bc_log.buffer, "\r\n");
+
+    bc_uart_write(BC_UART_UART2, _bc_log.buffer, strlen(_bc_log.buffer));
+}
+
+void bc_log_error(const char *format, ...)
+{
+    va_list ap;
+
+    if (_bc_log.level > BC_LOG_LEVEL_DEBUG)
+    {
+        return;
+    }
+
+    strcpy(_bc_log.buffer, "# <E> ");
+
+    va_start(ap, format);
+    vsnprintf(&_bc_log.buffer[6], sizeof(_bc_log.buffer) - 6 - 2, format, ap);
+    va_end(ap);
+
+    strcat(_bc_log.buffer, "\r\n");
+
+    bc_uart_write(BC_UART_UART2, _bc_log.buffer, strlen(_bc_log.buffer));
+}
