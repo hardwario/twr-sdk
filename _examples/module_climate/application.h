6341bd1c3673186434ddb5a58cf3e89880c214d3
berkas1 <berka@berkasimon.com>
2017-10-06 22:30:35 +0200

    add climate module simple example

---
+++ _examples/module_climate/application.h
@@ -0,0 +1,9 @@
+#ifndef _APPLICATION_H
+#define _APPLICATION_H
+
+#include <bcl.h>
+
+// Forward declarations
+void climate_event_handler(bc_module_climate_event_t event, void* params);
+
+#endif // _APPLICATION_H
