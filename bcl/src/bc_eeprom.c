bd1d4709ce2f1425714e0d00c1f61010319c2be3
Karel Blavka <karel.blavka@bigclown.com>
2017-10-20 12:32:47 +0200

    Fix eeprom write

--- bcl/src/bc_eeprom.c
+++ bcl/src/bc_eeprom.c
@@ -1,6 +1,9 @@
 #include <bc_eeprom.h>
 #include <bc_irq.h>
 #include <stm32l0xx.h>
+#include <bc_tick.h>
+
+static bool _bc_eeprom_is_busy(bc_tick_t timeout);
 
 bool bc_eeprom_write(uint32_t address, const void *buffer, size_t length)
 {
@@ -8,9 +11,14 @@ bool bc_eeprom_write(uint32_t address, const void *buffer, size_t length)
     address += DATA_EEPROM_BASE;
 
     // If user attempts to write outside EEPROM area...
-    if ((address + length) > (DATA_EEPROM_BANK2_END + 1UL))
+    if ((address + length) > (DATA_EEPROM_BANK2_END + 1))
     {
         // Indicate failure
+        return false;
+    }
+
+    if (_bc_eeprom_is_busy(50))
+    {
         return false;
     }
 
@@ -18,8 +26,11 @@ bool bc_eeprom_write(uint32_t address, const void *buffer, size_t length)
     bc_irq_disable();
 
     // Unlock FLASH_PECR register
-    FLASH->PEKEYR = FLASH_PEKEY1;
-    FLASH->PEKEYR = FLASH_PEKEY2;
+    if ((FLASH->PECR & FLASH_PECR_PELOCK) != 0)
+    {
+        FLASH->PEKEYR = FLASH_PEKEY1;
+        FLASH->PEKEYR = FLASH_PEKEY2;
+    }
 
     // Enable interrupts
     bc_irq_enable();
@@ -30,11 +41,15 @@ bool bc_eeprom_write(uint32_t address, const void *buffer, size_t length)
         // Program data to address
         ((uint8_t *) address)[i] = ((uint8_t *) buffer)[i];
 
-        // While memory interface busy flag is set...
-        while ((FLASH->SR & FLASH_SR_BSY) != 0UL)
+        if (_bc_eeprom_is_busy(20))
         {
-            continue;
+            return false;
         }
+    }
+
+    if (_bc_eeprom_is_busy(20))
+    {
+        return false;
     }
 
     // Disable interrupts
@@ -63,7 +78,7 @@ bool bc_eeprom_read(uint32_t address, void *buffer, size_t length)
     address += DATA_EEPROM_BASE;
 
     // If user attempts to read outside of EEPROM boundary...
-    if ((address + length) > (DATA_EEPROM_BANK2_END + 1UL))
+    if ((address + length) > (DATA_EEPROM_BANK2_END + 1))
     {
         // Indicate failure
         return false;
@@ -79,5 +94,20 @@ bool bc_eeprom_read(uint32_t address, void *buffer, size_t length)
 size_t bc_eeprom_get_size(void)
 {
     // Return EEPROM memory size
-    return DATA_EEPROM_BANK2_END - DATA_EEPROM_BASE + 1UL;
+    return DATA_EEPROM_BANK2_END - DATA_EEPROM_BASE + 1;
+}
+
+static bool _bc_eeprom_is_busy(bc_tick_t timeout)
+{
+    timeout += bc_tick_get();
+
+    while ((FLASH->SR & FLASH_SR_BSY) != 0UL)
+    {
+        if (timeout > bc_tick_get())
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
