9dd6e7f79e246f1a2931c527840d2ca738f90f6e
konnev <petr.konvicka@bigclown.com>
2017-11-13 12:06:41 +0100

    Fix empty parenthesis

--- bcl/inc/bc_module_core.h
+++ bcl/inc/bc_module_core.h
@@ -3,13 +3,13 @@
 
 #include <stdint.h>
 
-void bc_module_core_init();
-void bc_module_core_sleep();
-void bc_module_core_pll_enable();
-void bc_module_core_pll_disable();
+void bc_module_core_init(void);
+void bc_module_core_sleep(void);
+void bc_module_core_pll_enable(void);
+void bc_module_core_pll_disable(void);
 void bc_module_core_deep_sleep_disable(void);
 void bc_module_core_deep_sleep_enable(void);
-uint32_t bc_module_core_get_clk();
-void bc_module_core_reset();
+uint32_t bc_module_core_get_clk(void);
+void bc_module_core_reset(void);
 
 #endif // INC_BC_MODULE_CORE_H_
