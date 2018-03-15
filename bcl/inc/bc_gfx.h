b2347cd6016c82da288bd04b00d79738228d7b2b
Karel Blavka <karel.blavka@bigclown.com>
2018-03-15 16:02:43 +0100

    Add bc_gfx

---
+++ bcl/inc/bc_gfx.h
@@ -0,0 +1,187 @@
+#ifndef _BC_GFX_H
+#define _BC_GFX_H
+
+#include <bc_common.h>
+#include <bc_font_common.h>
+
+//! @addtogroup bc_gfx bc_gfx
+//! @brief Graphics library
+//! @{
+
+//! @brief Display size
+
+typedef struct
+{
+    int width;
+    int height;
+
+} bc_gfx_size_t;
+
+//! @brief Display driver interface
+
+typedef struct
+{
+    //! @brief Callback for is ready
+    bool (*is_ready)(void *self);
+
+    //! @brief Callback for clear display
+    void (*clear)(void *self);
+
+    //! @brief Callback for draw pifex
+    void (*draw_pixel)(void *self, int left, int top, uint32_t color);
+
+    //! @brief Callback for draw pifex
+    uint32_t (*get_pixel)(void *self, int left, int top);
+
+    //! @brief Callback for update
+    bool (*update)(void *self);
+
+    //! @brief Callback for get size
+    bc_gfx_size_t (*get_size)(void *self);
+
+} bc_gfx_driver_t;
+
+//! @brief Rotation
+
+typedef enum
+{
+    //! @brief Rotation 0 degrees
+    BC_GFX_ROTATION_0   = 0,
+
+    //! @brief Rotation 90 degrees
+    BC_GFX_ROTATION_90  = 1,
+
+    //! @brief Rotation 180 degrees
+    BC_GFX_ROTATION_180 = 2,
+
+    //! @brief Rotation 270 degrees
+    BC_GFX_ROTATION_270 = 3
+
+} bc_gfx_rotation_t;
+
+//! @brief Instance
+
+typedef struct
+{
+    void *_display;
+    const bc_gfx_driver_t *_driver;
+    bc_gfx_rotation_t _rotation;
+    const bc_font_t *_font;
+    bc_gfx_size_t _size;
+
+} bc_gfx_t;
+
+//! @brief Initialize button
+//! @param[in] self Instance
+
+void bc_gfx_init(bc_gfx_t *self, void *display, const bc_gfx_driver_t *driver);
+
+//! @brief Check if display driver is ready for commands
+//! @return true If ready
+//! @return false If not ready
+
+bool bc_gfx_display_is_ready(bc_gfx_t *self);
+
+//! @brief Clear
+//! @param[in] self Instance
+
+void bc_gfx_clear(bc_gfx_t *self);
+
+//! @brief Set font
+//! @param[in] self Instance
+//! @param[in] *font Font
+
+void bc_gfx_font_set(bc_gfx_t *self, const bc_font_t *font);
+
+//! @brief Set rotation
+//! @param[in] self Instance
+//! @param[in] rotation Rotation of diplay
+
+void bc_gfx_rotation_set(bc_gfx_t *self, bc_gfx_rotation_t rotation);
+
+//! @brief Get rotation
+//! @param[in] self Instance
+//! @return Rotation of display
+
+bc_gfx_rotation_t bc_gfx_rotation_get(bc_gfx_t *self);
+
+//! @brief Draw pixel
+//! @param[in] self Instance
+//! @param[in] left Pixels from left edge
+//! @param[in] top Pixels from top edge
+//! @param[in] color
+
+void bc_gfx_draw_pixel(bc_gfx_t *self, int x, int y, uint32_t color);
+
+//! @brief Display draw char
+//! @param[in] self Instance
+//! @param[in] left Pixels from left edge
+//! @param[in] top Pixels from top edge
+//! @param[in] ch Char to be printed
+//! @param[in] color
+//! @return Width of printed character
+
+int bc_gfx_draw_char(bc_gfx_t *self, int left, int top, uint8_t ch, uint32_t color);
+
+//! @brief Calc width character
+//! @param[in] self Instance
+//! @return Width of printed character
+
+int bc_gfx_calc_char(bc_gfx_t *self, uint8_t ch);
+
+//! @brief Display draw string
+//! @param[in] self Instance
+//! @param[in] left Pixels from left edge
+//! @param[in] top Pixels from top edge
+//! @param[in] *str String to be printed
+//! @param[in] color
+//! @return Width of printed string
+
+int bc_gfx_draw_string(bc_gfx_t *self, int left, int top, char *str, uint32_t color);
+
+//! @brief Calc width string
+//! @param[in] self Instance
+//! @param[in] *str String to be printed
+//! @return Width of printed string
+
+int bc_gfx_calc_string(bc_gfx_t *self,  char *str);
+
+//! @brief Display draw line
+//! @param[in] self Instance
+//! @param[in] x0 Pixels from left edge
+//! @param[in] y0 Pixels from top edge
+//! @param[in] x1 Pixels from left edge
+//! @param[in] y1 Pixels from top edge
+//! @param[in] color
+
+void bc_gfx_draw_line(bc_gfx_t *self, int x0, int y0, int x1, int y1, uint32_t color);
+
+//! @brief Display draw rectangle
+//! @param[in] self Instance
+//! @param[in] x0 Pixels from left edge
+//! @param[in] y0 Pixels from top edge
+//! @param[in] x1 Pixels from left edge
+//! @param[in] y1 Pixels from top edge
+//! @param[in] color
+
+void bc_gfx_draw_rectangle(bc_gfx_t *self, int x0, int y0, int x1, int y1, uint32_t color);
+
+//! @brief Lcd draw circle
+//! @param[in] self Instance
+//! @param[in] x0 Center - pixels from left edge
+//! @param[in] y0 Center - pixels from top edge
+//! @param[in] radius In pixels
+//! @param[in] color
+
+void bc_gfx_draw_circle(bc_gfx_t *self, int x0, int y0, int radius, uint32_t color);
+
+//! @brief Display update, send data
+//! @param[in] self Instance
+//! @return true On success
+//! @return false On failure
+
+bool bc_gfx_update(bc_gfx_t *self);
+
+//! @}
+
+#endif // _BC_GFX_H
---
+++ bcl/src/bc_gfx.c
@@ -0,0 +1,265 @@
+#include <bc_gfx.h>
+
+void bc_gfx_init(bc_gfx_t *self, void *display, const bc_gfx_driver_t *driver)
+{
+    memset(self, 0, sizeof(*self));
+    self->_display = display;
+    self->_driver = driver;
+    self->_font = &bc_font_ubuntu_15;
+
+    self->_size = driver->get_size(display);
+}
+
+bool bc_gfx_display_is_ready(bc_gfx_t *self)
+{
+    return self->_driver->is_ready(self->_display);
+}
+
+void bc_gfx_clear(bc_gfx_t *self)
+{
+    self->_driver->clear(self->_display);
+}
+
+void bc_gfx_font_set(bc_gfx_t *self, const bc_font_t *font)
+{
+    self->_font = font;
+}
+
+void bc_gfx_rotation_set(bc_gfx_t *self, bc_gfx_rotation_t rotation)
+{
+    self->_rotation = rotation;
+}
+
+bc_gfx_rotation_t bc_gfx_rotation_get(bc_gfx_t *self)
+{
+    return self->_rotation;
+}
+
+void bc_gfx_draw_pixel(bc_gfx_t *self, int x, int y, uint32_t color)
+{
+    if (x >= self->_size.width || y >= self->_size.height || x < 0 || y < 0)
+    {
+        return;
+    }
+
+    int tmp;
+
+    switch (self->_rotation)
+    {
+        case BC_GFX_ROTATION_90:
+        {
+            tmp = x;
+            x = 127 - y;
+            y = tmp;
+            break;
+        }
+        case BC_GFX_ROTATION_180:
+        {
+            x = 127 - x;
+            y = 127 - y;
+            break;
+        }
+        case BC_GFX_ROTATION_270:
+        {
+            tmp = y;
+            y = 127 - x;
+            x = tmp;
+            break;
+        }
+        case BC_GFX_ROTATION_0:
+        {
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+
+    self->_driver->draw_pixel(self->_display, x, y, color);
+}
+
+int bc_gfx_draw_char(bc_gfx_t *self, int left, int top, uint8_t ch, uint32_t color)
+{
+    const bc_font_t *font = self->_font;
+
+    int w = 0;
+    uint8_t h = 0;
+    uint16_t i;
+    uint16_t x;
+    uint16_t y;
+    uint8_t bytes;
+
+    for (i = 0; i < font->length; i++)
+    {
+        if (font->chars[i].code == ch)
+        {
+            w = font->chars[i].image->width;
+            h = font->chars[i].image->heigth;
+
+            bytes = (w + 7) / 8;
+
+            for (y = 0; y < h; y++)
+            {
+                for (x = 0; x < w; x++)
+                {
+                    uint32_t byteIndex = x / 8;
+                    byteIndex += y * bytes;
+
+                    uint8_t bitMask = 1 << (7 - (x % 8));
+
+                    if (font->chars[i].image->image[byteIndex] & bitMask)
+                    {
+                        bc_gfx_draw_pixel(self, left + x, top + y, 0);
+                    }
+                    else
+                    {
+                        bc_gfx_draw_pixel(self, left + x, top + y, color);
+                    }
+                }
+            }
+        }
+    }
+
+    return w;
+}
+
+int bc_gfx_calc_char(bc_gfx_t *self, uint8_t ch)
+{
+    const bc_font_t *font = self->_font;
+
+    for (int i = 0; i < font->length; i++)
+    {
+        if (font->chars[i].code == ch)
+        {
+            return font->chars[i].image->width;
+        }
+    }
+
+    return 0;
+}
+
+int bc_gfx_draw_string(bc_gfx_t *self, int left, int top, char *str, uint32_t color)
+{
+    while(*str)
+    {
+        left += bc_gfx_draw_char(self, left, top, *str, color);
+        str++;
+    }
+    return left;
+}
+
+int bc_gfx_calc_string(bc_gfx_t *self,  char *str)
+{
+    int width = 0;
+    while(*str)
+    {
+        width += bc_gfx_calc_char(self, *str);
+        str++;
+    }
+    return width;
+}
+
+void bc_gfx_draw_line(bc_gfx_t *self, int x0, int y0, int x1, int y1, uint32_t color)
+{
+    int16_t step = abs(y1 - y0) > abs(x1 - x0);
+    int16_t tmp;
+
+    if (step)
+    {
+        tmp = x0;
+        x0 = y0;
+        y0 = tmp;
+
+        tmp = x1;
+        x1 = y1;
+        y1 = tmp;
+    }
+
+    if (x0 > x1)
+    {
+        tmp = x0;
+        x0 = x1;
+        x1 = tmp;
+
+        tmp = y0;
+        y0 = y1;
+        y1 = tmp;
+    }
+
+    int16_t dx = x1 - x0;
+    int16_t dy = abs(y1 - y0);
+
+    int16_t err = dx / 2;
+    int16_t ystep;
+
+    ystep = y0 < y1 ? 1 : -1;
+
+    for (; x0 <= x1; x0++)
+    {
+        if (step)
+        {
+            bc_gfx_draw_pixel(self, y0, x0, color);
+        }
+        else
+        {
+            bc_gfx_draw_pixel(self, x0, y0, color);
+        }
+
+        err -= dy;
+
+        if (err < 0)
+        {
+            y0 += ystep;
+            err += dx;
+        }
+    }
+}
+
+void bc_gfx_draw_rectangle(bc_gfx_t *self, int x0, int y0, int x1, int y1, uint32_t color)
+{
+    bc_gfx_draw_line(self, x0, y0, x0, y1, color);
+    bc_gfx_draw_line(self, x0, y1, x1, y1, color);
+    bc_gfx_draw_line(self, x1, y0, x1, y1, color);
+    bc_gfx_draw_line(self, x1, y0, x0, y0, color);
+}
+
+// Using Midpoint circle algorithm
+void bc_gfx_draw_circle(bc_gfx_t *self, int x0, int y0, int radius, uint32_t color)
+{
+    int x = radius-1;
+    int y = 0;
+    int dx = 1;
+    int dy = 1;
+    int err = dx - (radius << 1);
+
+    while (x >= y)
+    {
+        bc_gfx_draw_pixel(self, x0 + x, y0 + y, color);
+        bc_gfx_draw_pixel(self, x0 + y, y0 + x, color);
+        bc_gfx_draw_pixel(self, x0 - y, y0 + x, color);
+        bc_gfx_draw_pixel(self, x0 - x, y0 + y, color);
+        bc_gfx_draw_pixel(self, x0 - x, y0 - y, color);
+        bc_gfx_draw_pixel(self, x0 - y, y0 - x, color);
+        bc_gfx_draw_pixel(self, x0 + y, y0 - x, color);
+        bc_gfx_draw_pixel(self, x0 + x, y0 - y, color);
+
+        if (err <= 0)
+        {
+            y++;
+            err += dy;
+            dy += 2;
+        }
+        if (err > 0)
+        {
+            x--;
+            dx += 2;
+            err += (-radius << 1) + dx;
+        }
+    }
+}
+
+bool bc_gfx_update(bc_gfx_t *self)
+{
+    return self->_driver->update(self->_display);
+}
