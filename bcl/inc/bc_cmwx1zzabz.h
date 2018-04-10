015ae396acf7350a107481414f5ab0344c479df9
hubmartin <hub.martin@gmail.com>
2018-04-10 19:53:08 +0200

    Add Doxygen tags

--- bcl/inc/bc_cmwx1zzabz.h
+++ bcl/inc/bc_cmwx1zzabz.h
@@ -6,7 +6,7 @@
 #include <bc_uart.h>
 
 //! @addtogroup bc_cmwx1zzabz bc_cmwx1zzabz
-//! @brief Driver for CMWX1ZZABZ SigFox modem
+//! @brief Driver for CMWX1ZZABZ muRata LoRa modem
 //! @{
 
 //! @cond
@@ -43,22 +43,6 @@ typedef enum
 
 } bc_cmwx1zzabz_event_t;
 
-//! @brief Save parameter index
-
-typedef enum
-{
-    BC_CMWX1ZZABZ_CONFIG_INDEX_DEVADDR = 0,
-    BC_CMWX1ZZABZ_CONFIG_INDEX_DEVEUI = 1,
-    BC_CMWX1ZZABZ_CONFIG_INDEX_APPEUI = 2,
-    BC_CMWX1ZZABZ_CONFIG_INDEX_NWKSKEY = 3,
-    BC_CMWX1ZZABZ_CONFIG_INDEX_APPSKEY = 4,
-    BC_CMWX1ZZABZ_CONFIG_INDEX_APPKEY = 5,
-    BC_CMWX1ZZABZ_CONFIG_INDEX_BAND = 6,
-    BC_CMWX1ZZABZ_CONFIG_INDEX_MODE = 7,
-    BC_CMWX1ZZABZ_CONFIG_INDEX_LAST_ITEM
-
-} bc_cmwx1zzabz_config_index_t;
-
 //! @brief CMWX1ZZABZ instance
 
 typedef struct bc_cmwx1zzabz_t bc_cmwx1zzabz_t;
@@ -84,6 +68,22 @@ typedef enum
     BC_CMWX1ZZABZ_CONFIG_BAND_US915 = 8
 
 } bc_cmwx1zzabz_config_band_t;
+
+//! @cond
+
+typedef enum
+{
+    BC_CMWX1ZZABZ_CONFIG_INDEX_DEVADDR = 0,
+    BC_CMWX1ZZABZ_CONFIG_INDEX_DEVEUI = 1,
+    BC_CMWX1ZZABZ_CONFIG_INDEX_APPEUI = 2,
+    BC_CMWX1ZZABZ_CONFIG_INDEX_NWKSKEY = 3,
+    BC_CMWX1ZZABZ_CONFIG_INDEX_APPSKEY = 4,
+    BC_CMWX1ZZABZ_CONFIG_INDEX_APPKEY = 5,
+    BC_CMWX1ZZABZ_CONFIG_INDEX_BAND = 6,
+    BC_CMWX1ZZABZ_CONFIG_INDEX_MODE = 7,
+    BC_CMWX1ZZABZ_CONFIG_INDEX_LAST_ITEM
+
+} bc_cmwx1zzabz_config_index_t;
 
 typedef enum
 {
@@ -145,7 +145,6 @@ struct bc_cmwx1zzabz_t
 
 //! @brief Initialize CMWX1ZZABZ
 //! @param[in] self Instance
-//! @param[in] reset_signal GPIO channel where RST signal is connected
 //! @param[in] uart_channel UART channel where TX and RX signals are connected
 
 void bc_cmwx1zzabz_init(bc_cmwx1zzabz_t *self, bc_uart_channel_t uart_channel);
@@ -167,35 +166,112 @@ bool bc_cmwx1zzabz_is_ready(bc_cmwx1zzabz_t *self);
 //! @brief Send RF frame command
 //! @param[in] self Instance
 //! @param[in] buffer Pointer to data to be transmitted
-//! @param[in] length Length of data to be transmitted in bytes (must be from 1 to 12 bytes)
+//! @param[in] length Length of data to be transmitted in bytes (must be from 1 to 51 bytes)
 //! @return true If command was accepted for processing
 //! @return false If command was denied for processing
 
 bool bc_cmwx1zzabz_send_message(bc_cmwx1zzabz_t *self, const void *buffer, size_t length);
 
+//! @brief Set DEVADDR
+//! @param[in] self Instance
+//! @param[in] devaddr Pointer to 8 character string
+
 void bc_cmwx1zzabz_set_devaddr(bc_cmwx1zzabz_t *self, char *devaddr);
+
+//! @brief Get DEVADDR
+//! @param[in] self Instance
+//! @param[in] devaddr Pointer to at least 8+1 character string
+
 void bc_cmwx1zzabz_get_devaddr(bc_cmwx1zzabz_t *self, char *devaddr);
 
+//! @brief Set DEVEUI
+//! @param[in] self Instance
+//! @param[in] deveui Pointer to 16 character string. WARNING, this value should not be changed as it is unique number programmed in the factory!
+
 void bc_cmwx1zzabz_set_deveui(bc_cmwx1zzabz_t *self, char *deveui);
+
+//! @brief Get DEVEUI
+//! @param[in] self Instance
+//! @param[in] deveui Pointer to at least 16+1 character string.
+
 void bc_cmwx1zzabz_get_deveui(bc_cmwx1zzabz_t *self, char *deveui);
 
+//! @brief Set APPEUI
+//! @param[in] self Instance
+//! @param[in] appeui Pointer to 16 character string
+
 void bc_cmwx1zzabz_set_appeui(bc_cmwx1zzabz_t *self, char *appeui);
+
+//! @brief Get APPEUI
+//! @param[in] self Instance
+//! @param[in] appeui Pointer to at least 16+1 character string
+
 void bc_cmwx1zzabz_get_appeui(bc_cmwx1zzabz_t *self, char *appeui);
 
+//! @brief Set NWKSKEY
+//! @param[in] self Instance
+//! @param[in] nwkskey Pointer to 32 character string
+
 void bc_cmwx1zzabz_set_nwkskey(bc_cmwx1zzabz_t *self, char *nwkskey);
+
+//! @brief Set NWKSKEY
+//! @param[in] self Instance
+//! @param[in] nwkskey Pointer to at least 32+1 character string
+
 void bc_cmwx1zzabz_get_nwkskey(bc_cmwx1zzabz_t *self, char *nwkskey);
 
+//! @brief Set APPSKEY
+//! @param[in] self Instance
+//! @param[in] appskey Pointer to 32 character string
+
 void bc_cmwx1zzabz_set_appskey(bc_cmwx1zzabz_t *self, char *appskey);
+
+//! @brief Get APPSKEY
+//! @param[in] self Instance
+//! @param[in] appskey Pointer to at least 32+1 character string
+
 void bc_cmwx1zzabz_get_appskey(bc_cmwx1zzabz_t *self, char *appskey);
 
+//! @brief Set APPKEY
+//! @param[in] self Instance
+//! @param[in] appkey Pointer to 32 character string
+
 void bc_cmwx1zzabz_set_appkey(bc_cmwx1zzabz_t *self, char *appkey);
+
+//! @brief Get APPKEY
+//! @param[in] self Instance
+//! @param[in] appkey Pointer to at least 32+1 character string
+
 void bc_cmwx1zzabz_get_appkey(bc_cmwx1zzabz_t *self, char *appkey);
 
+//! @brief Set BAND
+//! @param[in] self Instance
+//! @param[in] band Set correct frequency and modulation for EU, US and other countries
+
 void bc_cmwx1zzabz_set_band(bc_cmwx1zzabz_t *self, bc_cmwx1zzabz_config_band_t band);
+
+//! @brief Get BAND
+//! @param[in] self Instance
+//! @return Band value
+
 bc_cmwx1zzabz_config_band_t bc_cmwx1zzabz_get_band(bc_cmwx1zzabz_t *self);
 
+//! @brief Set ABP/OTAA mode
+//! @param[in] self Instance
+//! @param[in] mode ABP or OTAA mode
+
 void bc_cmwx1zzabz_set_mode(bc_cmwx1zzabz_t *self, bc_cmwx1zzabz_config_mode_t mode);
+
+//! @brief Get ABP/OTAA mode
+//! @param[in] self Instance
+//! @return ABP or OTAA mode
+
 bc_cmwx1zzabz_config_mode_t bc_cmwx1zzabz_get_mode(bc_cmwx1zzabz_t *self);
+
+//! @brief Start LoRa OTAA join procedure
+//! @param[in] self Instance
+//! @note The output of the join is handled by callback events
+//! @see bc_cmwx1zzabz_event_t
 
 void bc_cmwx1zzabz_join(bc_cmwx1zzabz_t *self);
 
