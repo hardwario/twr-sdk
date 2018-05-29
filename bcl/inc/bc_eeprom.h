8c5b7389bd250cd6e014b24b45ad67d1d23d2022
Karel Blavka <karel.blavka@bigclown.com>
2018-05-29 15:38:05 +0200

    Add async eeprom write

--- bcl/inc/bc_eeprom.h
+++ bcl/inc/bc_eeprom.h
@@ -7,6 +7,16 @@
 //! @brief Driver for internal EEPROM memory
 //! @{
 
+typedef enum
+{
+    //! @brief EEPROM event sync write error
+    BC_EEPROM_EVENT_ASYNC_WRITE_ERROR = 0,
+
+    //! @brief EEPROM event sync write done
+    BC_EEPROM_EVENT_ASYNC_WRITE_DONE = 1
+
+} bc_eepromc_event_t;
+
 //! @brief Write buffer to EEPROM area and verify it
 //! @param[in] address EEPROM start address (starts at 0)
 //! @param[in] buffer Pointer to source buffer
@@ -15,6 +25,21 @@
 //! @return false On failure
 
 bool bc_eeprom_write(uint32_t address, const void *buffer, size_t length);
+
+//! @brief Async write buffer to EEPROM area and verify it
+//! @param[in] address EEPROM start address (starts at 0)
+//! @param[in] buffer Pointer to source buffer
+//! @param[in] length Number of bytes to be written
+//! @param[in] event_handler Function address
+//! @param[in] event_param Optional event parameter (can be NULL)
+//! @return true On success start
+//! @return false On failure start
+
+bool bc_eeprom_async_write(uint32_t address, const void *buffer, size_t length, void (*event_handler)(bc_eepromc_event_t, void *), void *event_param);
+
+//! @brief Cancel async write
+
+void bc_eeprom_async_cancel(void);
 
 //! @brief Read buffer from EEPROM area
 //! @param[in] address EEPROM start address (starts at 0)
