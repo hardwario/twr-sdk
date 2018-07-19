068173d20c30e1124b68ecb172141589c8094f4a
Karel Blavka <karel.blavka@bigclown.com>
2018-07-19 14:58:16 +0200

    Update atsha204
    add bc_atsha204_lock
    add bc_atsha204_random
    add bc_atsha204_read

--- bcl/inc/bc_atsha204.h
+++ bcl/inc/bc_atsha204.h
@@ -5,11 +5,13 @@
 #include <bc_i2c.h>
 #include <bc_scheduler.h>
 
-#define BC_ATSHA204_RX_BUFFER_SIZE 32
-
 //! @addtogroup bc_atsha204 bc_atsha204
 //! @brief Driver for Atmel ATSHA204 CryptoAuthentication
 //! @{
+
+#define BC_ATSHA204_ZONE_CONFIG_LENGTH 88
+#define BC_ATSHA204_ZONE_OTP_LENGTH    64
+#define BC_ATSHA204_ZONE_DATA_LENGTH   512
 
 typedef enum
 {
@@ -20,9 +22,44 @@ typedef enum
     BC_ATSHA204_EVENT_REVISION_NUMBER = 1,
 
     //! @brief Event serial number is available
-    BC_ATSHA204_EVENT_SERIAL_NUMBER = 2
+    BC_ATSHA204_EVENT_SERIAL_NUMBER = 2,
+
+    //! @brief Event random value is available
+    BC_ATSHA204_EVENT_RANDOM = 3,
+
+    //! @brief Event read done
+    BC_ATSHA204_EVENT_READ_DONE = 4,
+
+    //! @brief Event lock done
+    BC_ATSHA204_EVENT_LOCK_DONE = 5,
+
+    //! @brief Event lock error
+    BC_ATSHA204_EVENT_LOCK_ERROR = 6,
 
 } bc_atsha204_event_t;
+
+typedef enum
+{
+    //! @brief Zone Config
+    BC_ATSHA204_ZONE_CONFIG = 0,
+
+    //! @brief Zone OTP
+    BC_ATSHA204_ZONE_OTP = 1,
+
+    //! @brief Zone Data
+    BC_ATSHA204_ZONE_DATA = 2,
+
+} bc_atsha204_zone_t;
+
+typedef enum
+{
+    //! @brief Lock Zone Config
+    BC_ATSHA204_LOCK_ZONE_CONFIG = 0,
+
+    //! @brief Lock zone OTP and Data
+    BC_ATSHA204_LOCK_ZONE_OTP_DATA = 1,
+
+} bc_atsha204_lock_zone_t;
 
 //! @brief ATSHA204 instance
 
@@ -35,7 +72,10 @@ typedef enum
     BC_ATSHA204_STATE_READY = 0,
     BC_ATSHA204_STATE_READ_SERIAL_NUMBER = 1,
     BC_ATSHA204_STATE_READ_SERIAL_NUMBER2 = 2,
-    BC_ATSHA204_STATE_SERIAL_NUMBER = 3
+    BC_ATSHA204_STATE_SERIAL_NUMBER = 3,
+    BC_ATSHA204_STATE_RANDOM = 4,
+    BC_ATSHA204_STATE_READ = 5,
+    BC_ATSHA204_STATE_LOCK = 6
 
 } bc_atsha204_state_t;
 
@@ -48,8 +88,14 @@ struct bc_atsha204_t
     bc_scheduler_task_id_t _task_id;
     bc_atsha204_state_t _state;
     bool _ready;
-    uint8_t _rx_buffer[BC_ATSHA204_RX_BUFFER_SIZE];
+    uint8_t _rx_buffer[32 + 3];
 
+    bc_atsha204_zone_t _zone;
+    uint32_t _address;
+    uint8_t *_buffer;
+    size_t _max_length;
+    size_t _length;
+    uint8_t _data_length;
 };
 
 //! @endcond
@@ -75,6 +121,14 @@ void bc_atsha204_set_event_handler(bc_atsha204_t *self, void (*event_handler)(bc
 
 bool bc_atsha204_is_ready(bc_atsha204_t *self);
 
+//! @brief Lock
+//! @param[in] self Instance
+//! @param[in] zone Zone
+//! @return true On success
+//! @return False When other command is in progress
+
+bool bc_atsha204_lock(bc_atsha204_t *self, bc_atsha204_lock_zone_t zone);
+
 //! @brief Reqeust for serial number
 //! @param[in] self Instance
 //! @return true On success
@@ -90,6 +144,24 @@ bool bc_atsha204_read_serial_number(bc_atsha204_t *self);
 //! @return false When value is invalid
 
 bool bc_atsha204_get_serial_number(bc_atsha204_t *self, void *destination, size_t size);
+
+void bc_atsha204_await(bc_atsha204_t *self);
+
+bool bc_atsha204_random(bc_atsha204_t *self);
+
+bool bc_atsha204_get_random(bc_atsha204_t *self, void *destination, size_t size);
+
+//! @brief Read buffer
+//! @param[in] self Instance
+//! @param[in] zone Zone
+//! @param[in] address EEPROM start address (starts at 0)
+//! @param[out] buffer Pointer to destination buffer
+//! @param[in] length Number of bytes to be read
+//! @return true On success
+//! @return false On failure
+
+bool bc_atsha204_read(bc_atsha204_t *self, bc_atsha204_zone_t zone, uint32_t address, void *buffer, size_t length);
+
 
 //! @}
 
--- bcl/src/bc_atsha204.c
+++ bcl/src/bc_atsha204.c
@@ -1,11 +1,15 @@
 #include <bc_atsha204.h>
 #include <bc_tick.h>
 
-#define _BC_ATSHA204_OPCODE_NULL  0x00
+#define _BC_ATSHA204_OPCODE_NULL   0x00
 #define _BC_ATSHA204_OPCODE_DEVREV 0x30
-#define _BC_ATSHA204_OPCODE_READ 0x02
+#define _BC_ATSHA204_OPCODE_READ   0x02
+#define _BC_ATSHA204_OPCODE_WRITE  0x12
+#define _BC_ATSHA204_OPCODE_RANDOM 0x1b
+#define _BC_ATSHA204_OPCODE_LOCK   0x17
 
 static void _bc_atsha204_task(void *param);
+static bool _bc_atsha204_command_read(bc_atsha204_t *self);
 static bool _bc_atsha204_send_command(bc_atsha204_t *self, uint8_t opcode, uint8_t param0, uint16_t param1);
 static void _bc_atsha204_wakeup_puls(bc_atsha204_t *self);
 static bool _bc_atsha204_wakeup(bc_atsha204_t *self);
@@ -37,9 +41,30 @@ bool bc_atsha204_is_ready(bc_atsha204_t *self)
     return self->_ready;
 }
 
+bool bc_atsha204_lock(bc_atsha204_t *self, bc_atsha204_lock_zone_t zone)
+{
+    if (!self->_ready)
+    {
+        return false;
+    }
+
+    if (!_bc_atsha204_send_command(self, _BC_ATSHA204_OPCODE_LOCK, (1 << 7) | zone, 0x0000))
+    {
+        return false;
+    }
+
+    self->_ready = false;
+
+    self->_state = BC_ATSHA204_STATE_LOCK;
+
+    bc_scheduler_plan_from_now(self->_task_id, 24);
+
+    return true;
+}
+
 bool bc_atsha204_read_serial_number(bc_atsha204_t *self)
 {
-    if (!bc_atsha204_is_ready(self))
+    if (!self->_ready)
     {
         return false;
     }
@@ -50,15 +75,39 @@ bool bc_atsha204_read_serial_number(bc_atsha204_t *self)
     }
 
     self->_ready = false;
+
     self->_state = BC_ATSHA204_STATE_READ_SERIAL_NUMBER;
 
-    bc_scheduler_plan_relative(self->_task_id, 4);
+    bc_scheduler_plan_from_now(self->_task_id, 4);
+
     return true;
+}
+
+void bc_atsha204_await(bc_atsha204_t *self)
+{
+    while (!self->_ready)
+    {
+        bc_tick_t tick = bc_scheduler_get_tick_execution(self->_task_id);
+
+        if (tick == BC_TICK_INFINITY)
+        {
+            return;
+        }
+
+        while (tick >= bc_tick_get())
+        {
+            continue;
+        }
+
+        bc_scheduler_plan_absolute(self->_task_id, BC_TICK_INFINITY);
+
+        _bc_atsha204_task(self);
+    }
 }
 
 bool bc_atsha204_get_serial_number(bc_atsha204_t *self, void *destination, size_t size)
 {
-    if (!bc_atsha204_is_ready(self) || self->_state != BC_ATSHA204_STATE_SERIAL_NUMBER)
+    if (!self->_ready || self->_state != BC_ATSHA204_STATE_SERIAL_NUMBER)
     {
         return false;
     }
@@ -85,6 +134,82 @@ bool bc_atsha204_get_serial_number(bc_atsha204_t *self, void *destination, size_
     return true;
 }
 
+
+bool bc_atsha204_random(bc_atsha204_t *self)
+{
+    if (!self->_ready)
+    {
+        return false;
+    }
+
+    if (!_bc_atsha204_send_command(self, _BC_ATSHA204_OPCODE_RANDOM, 0x00, 0x0000))
+    {
+        return false;
+    }
+
+    self->_ready = false;
+
+    self->_state = BC_ATSHA204_STATE_RANDOM;
+
+    bc_scheduler_plan_from_now(self->_task_id, 50);
+
+    return true;
+}
+
+bool bc_atsha204_get_random(bc_atsha204_t *self, void *destination, size_t size)
+{
+    if (!self->_ready || self->_state != BC_ATSHA204_STATE_RANDOM)
+    {
+        return false;
+    }
+
+    memcpy(destination, self->_rx_buffer + 1, size < 32 ? size : 32);
+
+    return true;
+}
+
+bool bc_atsha204_read(bc_atsha204_t *self, bc_atsha204_zone_t zone, uint32_t address, void *buffer, size_t length)
+{
+    if (!self->_ready || length == 0 || buffer == NULL || length > 512)
+    {
+        return false;
+    }
+
+    if (zone == BC_ATSHA204_ZONE_CONFIG)
+    {
+        self->_max_length = length > 88 ? 88 : length;
+    }
+    else if (zone == BC_ATSHA204_ZONE_OTP)
+    {
+        self->_max_length = length > 64 ? 64 : length;
+    }
+    else
+    {
+        self->_max_length = length;
+    }
+
+    self->_address = address;
+
+    self->_zone = zone;
+
+    self->_buffer = buffer;
+
+    self->_length = 0;
+
+    if (!_bc_atsha204_command_read(self))
+    {
+        return false;
+    }
+
+    self->_ready = false;
+
+    self->_state = BC_ATSHA204_STATE_READ;
+
+    bc_scheduler_plan_from_now(self->_task_id, 4);
+
+    return true;
+}
+
 static void _bc_atsha204_task(void *param)
 {
     bc_atsha204_t *self = param;
@@ -105,7 +230,9 @@ static void _bc_atsha204_task(void *param)
             }
 
             self->_state = BC_ATSHA204_STATE_READ_SERIAL_NUMBER2;
+
             bc_scheduler_plan_current_from_now(4);
+
             return;
         }
         case BC_ATSHA204_STATE_READ_SERIAL_NUMBER2:
@@ -115,8 +242,58 @@ static void _bc_atsha204_task(void *param)
                 break;
             }
 
-            self->_state = BC_ATSHA204_STATE_SERIAL_NUMBER;
             event = BC_ATSHA204_EVENT_SERIAL_NUMBER;
+
+            break;
+        }
+        case BC_ATSHA204_STATE_RANDOM:
+        {
+            if (!_bc_atsha204_read(self, self->_rx_buffer, 32 + 3))
+            {
+                break;
+            }
+
+            event = BC_ATSHA204_EVENT_RANDOM;
+
+            break;
+        }
+        case BC_ATSHA204_STATE_READ:
+        {
+            if (!_bc_atsha204_read(self, self->_rx_buffer, self->_data_length + 3))
+            {
+                break;
+            }
+
+            size_t length = (self->_max_length - self->_length) < self->_data_length ? (self->_max_length - self->_length) : self->_data_length;
+
+            memcpy(self->_buffer + self->_length, self->_rx_buffer + 1, length);
+
+            self->_length += length;
+
+            if (self->_max_length == self->_length)
+            {
+                event = BC_ATSHA204_EVENT_READ_DONE;
+
+                break;
+            }
+
+            if (!_bc_atsha204_command_read(self))
+            {
+                break;
+            }
+
+            bc_scheduler_plan_from_now(self->_task_id, 4);
+
+            return;
+        }
+        case BC_ATSHA204_STATE_LOCK:
+        {
+            if (!_bc_atsha204_read(self, self->_rx_buffer, 4))
+            {
+                break;
+            }
+
+            event = self->_rx_buffer[1] == 0 ? BC_ATSHA204_EVENT_LOCK_DONE : BC_ATSHA204_EVENT_LOCK_ERROR;
 
             break;
         }
@@ -134,8 +311,26 @@ static void _bc_atsha204_task(void *param)
     {
         self->_event_handler(self, event, self->_event_param);
     }
+}
 
+static bool _bc_atsha204_command_read(bc_atsha204_t *self)
+{
+    size_t length = self->_max_length - self->_length;
 
+    uint8_t address_mod = self->_address % 32;
+
+    self->_data_length = (length > 31 && address_mod == 0) ? 32 : 4;
+
+    uint16_t param1 = ((self->_address / 32) << 3) | (address_mod / 4);
+
+    if (!_bc_atsha204_send_command(self, _BC_ATSHA204_OPCODE_READ, (self->_data_length == 32 ? 1 << 7 : 0) | self->_zone, param1))
+    {
+        return false;
+    }
+
+    self->_address += self->_data_length;
+
+    return true;
 }
 
 static bool _bc_atsha204_send_command(bc_atsha204_t *self, uint8_t opcode, uint8_t param0, uint16_t param1)
