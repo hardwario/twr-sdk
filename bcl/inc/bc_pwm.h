ec39fcdf63f3f6509e19daead3c109ff944965c3
hubmartin <hub.martin@gmail.com>
2018-07-31 15:14:25 +0200

    Add 2 additional PWM on TIM21, bc_delay timer moved to TIM22

--- bcl/inc/bc_pwm.h
+++ bcl/inc/bc_pwm.h
@@ -5,62 +5,66 @@
 #include <bc_gpio.h>
 #include <stm32l0xx.h>
 
-/* example code
-
-    For Pavel: will be removed in the final version ;)
-
-    bc_pwm_init(BC_GPIO_P0);
-    bc_pwm_set(BC_GPIO_P0, 10);
-    bc_pwm_enable(BC_GPIO_P0);
-
-    bc_pwm_init(BC_GPIO_P1);
-    bc_pwm_set(BC_GPIO_P1, 40);
-    bc_pwm_enable(BC_GPIO_P1);
-
-    bc_pwm_init(BC_GPIO_P2);
-    bc_pwm_set(BC_GPIO_P2, 80);
-    bc_pwm_enable(BC_GPIO_P2);
-
-    bc_pwm_init(BC_GPIO_P3);
-    bc_pwm_set(BC_GPIO_P3, 120);
-    bc_pwm_enable(BC_GPIO_P3);
-
-    // TIM3
-    bc_pwm_init(BC_GPIO_P6);
-    bc_pwm_set(BC_GPIO_P6, 180);
-    bc_pwm_enable(BC_GPIO_P6);
-
-    bc_pwm_init(BC_GPIO_P7);
-    bc_pwm_set(BC_GPIO_P7, 210);
-    bc_pwm_enable(BC_GPIO_P7);
-
-    bc_pwm_init(BC_GPIO_P8);
-    bc_pwm_set(BC_GPIO_P8, 255);
-    bc_pwm_enable(BC_GPIO_P8);
-
-*/
-
 //! @addtogroup bc_pwm bc_pwm
-//! @brief Driver for timer
+//! @brief Driver for PWM generation
 //! @{
 
-//! @brief Initialize PWM timers
+//! @brief PWM channels
 
-void bc_pwm_init(bc_gpio_channel_t channel);
+typedef enum
+{
+    BC_PWM_P0 = BC_GPIO_P0,
+    BC_PWM_P1 = BC_GPIO_P1,
+    BC_PWM_P2 = BC_GPIO_P2,
+    BC_PWM_P3 = BC_GPIO_P3,
 
-//! @brief Enable PWM output
+    BC_PWM_P6 = BC_GPIO_P6,
+    BC_PWM_P7 = BC_GPIO_P7,
+    BC_PWM_P8 = BC_GPIO_P8,
 
-void bc_pwm_enable(bc_gpio_channel_t channel);
+    BC_PWM_P12 = BC_GPIO_P12,
+    BC_PWM_P14 = BC_GPIO_P14,
 
-//! @brief Set PWM value
+} bc_pwm_channel_t;
 
-void bc_pwm_set(bc_gpio_channel_t channel, uint16_t pwm_value);
+//! @brief Initialize PWM timers based on GPIO pin
+//! @param[in] channel GPIO channel
 
-void bc_pwm_tim3_init(uint32_t resolution_us, uint32_t period_cycles);
+void bc_pwm_init(bc_pwm_channel_t channel);
 
-void bc_pwm_tim2_init(uint32_t resolution_us, uint32_t period_cycles);
+//! @brief Enable PWM output on GPIO pin
+//! @param[channel] channel GPIO channel
 
+void bc_pwm_enable(bc_pwm_channel_t channel);
 
+//! @brief Disable PWM output on GPIO pin
+//! @param[channel] channel GPIO channel
+
+void bc_pwm_disable(bc_pwm_channel_t channel);
+
+//! @brief Set PWM value for GPIO pin
+//! @param[channel] channel GPIO channel
+//! @param[pwm_value] PWM duty value 0-255 in default configuration
+
+void bc_pwm_set(bc_pwm_channel_t channel, uint16_t pwm_value);
+
+//! @brief Reconfigure TIM3
+//! @param[resolution_us] Set resolution of the timer in microseconds
+//! @param[period_cycles] Set number of periods of one cycle
+
+void bc_pwm_tim3_configure(uint32_t resolution_us, uint32_t period_cycles);
+
+//! @brief Reconfigure TIM2
+//! @param[resolution_us] Set resolution of the timer in microseconds
+//! @param[period_cycles] Set number of periods of one cycle
+
+void bc_pwm_tim2_configure(uint32_t resolution_us, uint32_t period_cycles);
+
+//! @brief Reconfigure TIM21
+//! @param[resolution_us] Set resolution of the timer in microseconds
+//! @param[period_cycles] Set number of periods of one cycle
+
+void bc_pwm_tim21_configure(uint32_t resolution_us, uint32_t period_cycles);
 
 //! @}
 
--- bcl/src/bc_pwm.c
+++ bcl/src/bc_pwm.c
@@ -1,28 +1,30 @@
 #include <bc_pwm.h>
 
 /*
-P0 PA0 TIM2_CH1
-P1 PA1 TIM2_CH2
-P2 PA2 TIM2_CH3 TIM21_CH1
-P3 PA3 TIM2_CH4 TIM21_CH2
-P4 PA4 none
-P5 PA5 TIM2_CH1
-P6 PB1 TIM3_CH4
-P7 PA6 TIM3_CH1 TIM22_CH2
-P8 PB0 TIM3_CH3
-P9 PB2 LPTIM1_OUT
+    Table of pins and TIM channels
 
-P10 PA10 none
-P11 PA9 none
-P12 PB14 TIM21_CH2
-P13 PB15 none
-P14 PB13 TIM21_CH1
-P15 PB12 none
-P16 PB8 none
-P17 PB9 none
+    P0 PA0 TIM2_CH1
+    P1 PA1 TIM2_CH2
+    P2 PA2 TIM2_CH3 TIM21_CH1
+    P3 PA3 TIM2_CH4 TIM21_CH2
+    P4 PA4 none
+    P5 PA5 TIM2_CH1
+    P6 PB1 TIM3_CH4
+    P7 PA6 TIM3_CH1 TIM22_CH2
+    P8 PB0 TIM3_CH3
+    P9 PB2 LPTIM1_OUT
+
+    P10 PA10 none
+    P11 PA9 none
+    P12 PB14 TIM21_CH2
+    P13 PB15 none
+    P14 PB13 TIM21_CH1
+    P15 PB12 none
+    P16 PB8 none
+    P17 PB9 none
 */
 
-void bc_pwm_tim2_init(uint32_t resolution_us, uint32_t period_cycles)
+void bc_pwm_tim2_configure(uint32_t resolution_us, uint32_t period_cycles)
 {
     // Enable TIM2 clock
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
@@ -35,8 +37,8 @@ void bc_pwm_tim2_init(uint32_t resolution_us, uint32_t period_cycles)
 
     // Set prescaler to 5 * 32 (5 microseconds resolution)
     //TIM2->PSC = 5 * 32 - 1;
-    TIM2->PSC = (resolution_us * 32) - 1;
-    TIM2->ARR= period_cycles - 1;
+    TIM2->PSC = resolution_us * 32 - 1;
+    TIM2->ARR = period_cycles - 1;
 
     // CH1
     TIM2->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
@@ -54,8 +56,7 @@ void bc_pwm_tim2_init(uint32_t resolution_us, uint32_t period_cycles)
     TIM2->CR1 |= TIM_CR1_CEN;
 }
 
-
-void bc_pwm_tim3_init(uint32_t resolution_us, uint32_t period_cycles)
+void bc_pwm_tim3_configure(uint32_t resolution_us, uint32_t period_cycles)
 {
     // Enable TIM3 clock
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
@@ -68,12 +69,12 @@ void bc_pwm_tim3_init(uint32_t resolution_us, uint32_t period_cycles)
 
     // Set prescaler to 5 * 32 (5 microseconds resolution)
     TIM3->PSC = resolution_us * 32 - 1;
-    TIM3->ARR= period_cycles - 1;
+    TIM3->ARR = period_cycles - 1;
 
     // CH1
     TIM3->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
     TIM3->CCER |= TIM_CCER_CC1E;
-    // CH2
+    // CH2 - is on PB5 - RADIO_MOSI
     //TIM3->CCMR1 |= TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2;
     //TIM3->CCER |= TIM_CCER_CC2E;
     // CH3
@@ -86,11 +87,36 @@ void bc_pwm_tim3_init(uint32_t resolution_us, uint32_t period_cycles)
     TIM3->CR1 |= TIM_CR1_CEN;
 }
 
+void bc_pwm_tim21_configure(uint32_t resolution_us, uint32_t period_cycles)
+{
+    // Enable TIM21 clock
+    RCC->APB2ENR |= RCC_APB2ENR_TIM21EN;
 
-void bc_pwm_init(bc_gpio_channel_t channel)
+    // Errata workaround
+    RCC->APB2ENR;
+
+    // Disable counter if it is running
+    TIM21->CR1 &= ~TIM_CR1_CEN;
+
+    // Set prescaler to 5 * 32 (5 microseconds resolution)
+    TIM21->PSC = resolution_us * 32 - 1;
+    TIM21->ARR = period_cycles - 1;
+
+    // CH1
+    TIM21->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+    TIM21->CCER |= TIM_CCER_CC1E;
+    // CH2
+    TIM21->CCMR1 |= TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2;
+    TIM21->CCER |= TIM_CCER_CC2E;
+
+    TIM21->CR1 |= TIM_CR1_CEN;
+}
+
+void bc_pwm_init(bc_pwm_channel_t channel)
 {
     static bool tim2_initialized = false;
     static bool tim3_initialized = false;
+    static bool tim21_initialized = false;
     static bool pll_enabled = false;
 
     if (!pll_enabled)
@@ -99,74 +125,97 @@ void bc_pwm_init(bc_gpio_channel_t channel)
         pll_enabled = true;
     }
 
-    if (!tim2_initialized && (channel == BC_GPIO_P0 || channel == BC_GPIO_P1 || channel == BC_GPIO_P2 || channel == BC_GPIO_P3))
+    if (!tim2_initialized && (channel == BC_PWM_P0 || channel == BC_PWM_P1 || channel == BC_PWM_P2 || channel == BC_PWM_P3))
     {
-        bc_pwm_tim2_init(5, 255);
+        // 5 us * 255 = cca 784 Hz
+        bc_pwm_tim2_configure(5, 255);
         tim2_initialized = true;
     }
 
-    if (!tim3_initialized && (channel == BC_GPIO_P6 || channel == BC_GPIO_P7 || channel == BC_GPIO_P8))
+    if (!tim3_initialized && (channel == BC_PWM_P6 || channel == BC_PWM_P7 || channel == BC_PWM_P8))
     {
-        // 5 us * 255 = cca 784 Hz
-        bc_pwm_tim3_init(5, 255);
+        bc_pwm_tim3_configure(5, 255);
         tim3_initialized = true;
+    }
+
+    if (!tim21_initialized && (channel == BC_PWM_P12 || channel == BC_PWM_P14))
+    {
+        bc_pwm_tim21_configure(5, 255);
+        tim21_initialized = true;
     }
 }
 
-void bc_pwm_enable(bc_gpio_channel_t channel)
+void bc_pwm_enable(bc_pwm_channel_t channel)
 {
     bc_gpio_init(channel);
-    bc_gpio_set_mode(channel, BC_GPIO_MODE_ALTERNATE_2);
+
+    if (channel == BC_PWM_P12 || channel == BC_PWM_P14)
+    {
+        bc_gpio_set_mode(channel, BC_GPIO_MODE_ALTERNATE_6);
+    }
+    else
+    {
+        bc_gpio_set_mode(channel, BC_GPIO_MODE_ALTERNATE_2);
+    }
 }
 
-void bc_pwm_set(bc_gpio_channel_t channel, uint16_t pwm_value)
+void bc_pwm_disable(bc_pwm_channel_t channel)
+{
+    bc_gpio_set_mode(channel, BC_GPIO_MODE_ANALOG);
+}
+
+void bc_pwm_set(bc_pwm_channel_t channel, uint16_t pwm_value)
 {
     switch (channel)
     {
-        case BC_GPIO_P0:
-        TIM2->CCR1 = pwm_value;
-        break;
-
-        case BC_GPIO_P1:
-        TIM2->CCR2 = pwm_value;
-        break;
-
-        case BC_GPIO_P2:
-        TIM2->CCR3 = pwm_value;
-        break;
-
-        case BC_GPIO_P3:
-        TIM2->CCR4 = pwm_value;
-        break;
-
-    case BC_GPIO_P4:
-    case BC_GPIO_P5:
-        break;
-
-        case BC_GPIO_P6:
-        TIM3->CCR4 = pwm_value;
-        break;
-
-        case BC_GPIO_P7:
-        TIM3->CCR1 = pwm_value;
-        break;
-
-        case BC_GPIO_P8:
-        TIM3->CCR3 = pwm_value;
-        break;
-
-        case BC_GPIO_P9:
-        case BC_GPIO_P10:
-        case BC_GPIO_P11:
-        case BC_GPIO_P12:
-        case BC_GPIO_P13:
-        case BC_GPIO_P14:
-        case BC_GPIO_P15:
-        case BC_GPIO_P16:
-        case BC_GPIO_P17:
-        case BC_GPIO_LED:
-        case BC_GPIO_BUTTON:
+        case BC_PWM_P0:
+        {
+            TIM2->CCR1 = pwm_value;
+            break;
+        }
+        case BC_PWM_P1:
+        {
+            TIM2->CCR2 = pwm_value;
+            break;
+        }
+        case BC_PWM_P2:
+        {
+            TIM2->CCR3 = pwm_value;
+            break;
+        }
+        case BC_PWM_P3:
+        {
+            TIM2->CCR4 = pwm_value;
+            break;
+        }
+        case BC_PWM_P6:
+        {
+            TIM3->CCR4 = pwm_value;
+            break;
+        }
+        case BC_PWM_P7:
+        {
+            TIM3->CCR1 = pwm_value;
+            break;
+        }
+        case BC_PWM_P8:
+        {
+            TIM3->CCR3 = pwm_value;
+            break;
+        }
+        case BC_PWM_P12:
+        {
+            TIM21->CCR2 = pwm_value;
+            break;
+        }
+        case BC_PWM_P14:
+        {
+            TIM21->CCR1 = pwm_value;
+            break;
+        }
         default:
-        break;
+        {
+            break;
+        }
     }
 }
