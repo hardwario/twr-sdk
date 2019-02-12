edaa8e8b4754ae5d34ac55b01602d8ff888089fd
hubmartin <hub.martin@gmail.com>
2019-02-12 17:38:11 +0100

    Export/add functions for manual 1-wire search

--- bcl/inc/bc_onewire.h
+++ bcl/inc/bc_onewire.h
@@ -99,6 +99,18 @@ int bc_onewire_search_all(bc_gpio_channel_t channel, uint64_t *device_list, size
 
 int bc_onewire_search_family(bc_gpio_channel_t channel, uint8_t family_code, uint64_t *device_list, size_t device_list_size);
 
+//! @brief Start of manual search, see also bc_onewire_search_next()
+//! @param[in] family_code Family code of 1-Wire device or NULL
+
+void bc_onewire_search_start(uint8_t family_code);
+
+//! @brief Manual search of next device
+//! @param[in] device_number GPIO channel
+//! @param[in] device_number 64b device number
+//! @return true if new device was found, false if ther are no more devices on the bus
+
+bool bc_onewire_search_next(bc_gpio_channel_t channel, uint64_t *device_number);
+
 //! @brief Calculate 8-bit CRC
 //! @param[in] buffer
 //! @param[in] length Number of bytes
