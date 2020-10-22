4925ca4c4ca635a37acccf252d65c3c92787196c
Karel Blavka <karel.blavka@hardwario.com>
2020-10-23 01:34:47 +0200

    Add bc_delay_us

---
+++ bcl/inc/bc_delay.h
@@ -0,0 +1,17 @@
+#ifndef _BC_DELAY_H
+#define _BC_DELAY_H
+
+#include <bc_common.h>
+
+//! @addtogroup bc_delay bc_delay
+//! @brief Library for delay
+//! @{
+
+//! @brief Delay us
+//! @param[in] microseconds Time to delay in us
+
+void bc_delay_us(uint16_t microseconds);
+
+//! @}
+
+#endif // _BC_DELAY_H
