6bd4ea38286d989969212718422b8f945d9c9f94
Karel Blavka <karel.blavka@hardwario.com>
2020-11-29 10:02:08 +0100

    Update main.c

--- hio/stm/src/main.c
+++ hio/stm/src/main.c
@@ -1,32 +1,32 @@
-#include <bc_scheduler.h>
-#include <bc_system.h>
-#include <bc_error.h>
-#include <bc_log.h>
-#include <bc_led.h>
-#include <bc_timer.h>
+#include <hio_scheduler.h>
+#include <hio_system.h>
+#include <hio_error.h>
+#include <hio_log.h>
+#include <hio_led.h>
+#include <hio_timer.h>
 
 void application_init(void);
 
 void application_task(void *param);
 
-void application_error(bc_error_t code);
+void application_error(hio_error_t code);
 
 int main(void)
 {
-    bc_system_init();
+    hio_system_init();
 
-    while (bc_tick_get() < 500)
+    while (hio_tick_get() < 500)
     {
         continue;
     }
 
-    bc_scheduler_init();
+    hio_scheduler_init();
 
-    bc_scheduler_register(application_task, NULL, 0);
+    hio_scheduler_register(application_task, NULL, 0);
 
     application_init();
 
-    bc_scheduler_run();
+    hio_scheduler_run();
 }
 
 __attribute__((weak)) void application_init(void)
@@ -38,25 +38,25 @@ __attribute__((weak)) void application_task(void *param)
     (void) param;
 }
 
-__attribute__((weak)) void application_error(bc_error_t code)
+__attribute__((weak)) void application_error(hio_error_t code)
 {
 #ifdef RELEASE
 
     (void) code;
 
-    bc_system_reset();
+    hio_system_reset();
 
 #else
 
-    bc_log_init(BC_LOG_LEVEL_DEBUG, BC_LOG_TIMESTAMP_ABS);
+    hio_log_init(HIO_LOG_LEVEL_DEBUG, HIO_LOG_TIMESTAMP_ABS);
 
-    bc_led_t led;
+    hio_led_t led;
 
-    bc_tick_t timeout = 0;
+    hio_tick_t timeout = 0;
 
     int cnt = 0;
 
-    bc_led_init(&led, BC_GPIO_LED, false, false);
+    hio_led_init(&led, HIO_GPIO_LED, false, false);
 
     while (true)
     {
@@ -64,24 +64,24 @@ __attribute__((weak)) void application_error(bc_error_t code)
         {
             switch (code)
             {
-                case BC_ERROR_NOT_ENOUGH_TASKS:
+                case HIO_ERROR_NOT_ENOUGH_TASKS:
                 {
-                    bc_log_error("BC_ERROR_NOT_ENOUGH_TASKS");
+                    hio_log_error("HIO_ERROR_NOT_ENOUGH_TASKS");
                     break;
                 }
-                case BC_ERROR_LOG_NOT_INITIALIZED:
+                case HIO_ERROR_LOG_NOT_INITIALIZED:
                 {
-                    bc_log_error("BC_ERROR_LOG_NOT_INITIALIZED");
+                    hio_log_error("HIO_ERROR_LOG_NOT_INITIALIZED");
                     break;
                 }
-                case BC_ERROR_ERROR_UNLOCK:
+                case HIO_ERROR_ERROR_UNLOCK:
                 {
-                    bc_log_error("BC_ERROR_ERROR_UNLOCK");
+                    hio_log_error("HIO_ERROR_ERROR_UNLOCK");
                     break;
                 }
-                case BC_ERROR_CALLBACK:
+                case HIO_ERROR_CALLBACK:
                 {
-                    bc_log_error("BC_ERROR_CALLBACK");
+                    hio_log_error("HIO_ERROR_CALLBACK");
                     break;
                 }
                 default:
@@ -91,20 +91,20 @@ __attribute__((weak)) void application_error(bc_error_t code)
             }
         }
 
-        bc_led_set_mode(&led, BC_LED_MODE_ON);
+        hio_led_set_mode(&led, HIO_LED_MODE_ON);
 
-        timeout = bc_tick_get() + ((cnt > 1 && cnt < 5) ? 1000 : 300);
+        timeout = hio_tick_get() + ((cnt > 1 && cnt < 5) ? 1000 : 300);
 
-        while (timeout > bc_tick_get())
+        while (timeout > hio_tick_get())
         {
             continue;
         }
 
-        bc_led_set_mode(&led, BC_LED_MODE_OFF);
+        hio_led_set_mode(&led, HIO_LED_MODE_OFF);
 
-        timeout = bc_tick_get() + (cnt == 7 ? 2000 : 300);
+        timeout = hio_tick_get() + (cnt == 7 ? 2000 : 300);
 
-        while (timeout > bc_tick_get())
+        while (timeout > hio_tick_get())
         {
             continue;
         }
