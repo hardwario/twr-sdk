24e5a9543d6cb2173e60bbecc123deffdbca77f4
hubmartin <hub.martin@gmail.com>
2021-12-08 10:10:58 +0100

    Move examples from SDK to PlatformIO platform

--- _examples/module_climate/application.h
+++
@@ -1,9 +0,0 @@
-#ifndef _APPLICATION_H
-#define _APPLICATION_H
-
-#include <twr.h>
-
-// Forward declarations
-void climate_event_handler(twr_module_climate_event_t event, void* params);
-
-#endif // _APPLICATION_H
