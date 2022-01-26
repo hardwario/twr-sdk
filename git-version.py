de85798a57d1f986eebae66730bbbc931b7ca118
Jan Janak <jan@janakj.org>
2022-01-26 12:46:06 -0500

    Add preprocessor macro VERSION
    The string in VERSION will be set to the most recent tag or to "vdev" if
    no tag was found. Git commit hash abbreviations are not used.

--- git-version.py
+++ git-version.py
@@ -2,10 +2,15 @@ from subprocess import check_output
 from datetime import datetime
 
 try:
+    version=check_output(['git', 'describe', '--tags', '--abbrev=0', '--dirty=m']).decode().strip()
+except:
+    version='vdev'
+
+try:
     git_version=check_output(['git', 'describe', '--abbrev=8', '--always', '--tags', '--dirty= (modified)']).decode().strip()
 except:
     git_version='?'
 
 build_date=datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
 
-print('-D GIT_VERSION="\\"%s\\"" -D BUILD_DATE="\\"%s\\""' % (git_version, build_date))
+print('-D VERSION="\\"%s\\"" -D GIT_VERSION="\\"%s\\"" -D BUILD_DATE="\\"%s\\""' % (version, git_version, build_date))
